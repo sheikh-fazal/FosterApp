d64ff27d0ec2c3557de4605ac121dc66553072e3
Ijaz Liaqat <ijaz.liaqat@ceative.co.uk>
2023-05-22 17:27:31 +0500

    changes in assessment stage one page

---
+++ src/sections/recruitment/assessment-stage-one/training-verification-form/edit-training-profile/index.tsx
@@ -0,0 +1,69 @@
+import Page from "@root/components/Page";
+import Layout from "@root/layouts";
+import HomeIcon from "@mui/icons-material/Home";
+import HorizaontalTabs from "@root/components/HorizaontalTabs";
+import RecruitmentTrainingProfile from "@root/sections/recruitment/assessment-stage-one/training-verification-form/add-taining-profile/training-profile/RecruitmentTrainingProfile";
+import RecruitmentUploadDocuments from "@root/sections/recruitment/assessment-stage-one/training-verification-form/add-taining-profile/upload-documents/RecruitmentUploadDocuments";
+import EditTrainingProfile from "@root/sections/recruitment/assessment-stage-one/training-verification-form/edit-training-profile/EditTrainingProfile";
+import { useRouter } from "next/router";
+import { useGetSingleTrainingProfileDataQuery } from "@root/services/recruitment/assessment-stage-one/training-verification-form/TrainingProfileAllApi";
+import IsFetching from "@root/components/loaders/IsFetching";
+import dayjs from "dayjs";
+
+const PAGE_TITLE = "Recruitment";
+
+AddTraingVerification.getLayout = function getLayout(page: any) {
+  return (
+    <Layout
+      showTitleWithBreadcrumbs
+      breadcrumbs={[
+        {
+          icon: <HomeIcon />,
+          name: "Assessment stage 1",
+          href: "/recruitment",
+        },
+        {
+          name: "Training Profile",
+        },
+      ]}
+      title={PAGE_TITLE}
+    >
+      {page}
+    </Layout>
+  );
+};
+
+export default function AddTraingVerification() {
+  const router = useRouter();
+  const id = Object.keys(router?.query)[0];
+
+  const { data, isLoading, isError, isFetching, isSuccess } =
+    useGetSingleTrainingProfileDataQuery(id);
+
+    console.log(data?.data?.expiryDate, "single trining profile");
+
+  return (
+    <Page title={PAGE_TITLE}>
+      <HorizaontalTabs tabsDataArray={["Training Profile", "Upload Documents"]}>
+        {isLoading ? (
+          <IsFetching isFetching={isLoading} />
+        ) : (
+          <>
+            <EditTrainingProfile
+              initialValueProps={{
+                ...data,
+                expiryDate: new Date(data?.data?.expiryDate),
+                date: new Date(data?.data?.date),
+              }}
+              //   onSubmitHandler={updateData}
+              message={"Updated"}
+              isError={isError}
+              isSuccess={isSuccess}
+            />
+          </>
+        )}
+        <RecruitmentUploadDocuments />
+      </HorizaontalTabs>
+    </Page>
+  );
+}
