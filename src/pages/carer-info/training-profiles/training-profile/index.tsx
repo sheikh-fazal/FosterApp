d5a4b2cb4d54a930e2439673c2e88273b2b7aa60
zaka <zaka.ullah@ceative.co.uk>
2023-05-23 21:59:21 +0500

    create training profile component

--- src/pages/carer-info/training-profiles/training-profile/index.tsx
+++ src/pages/carer-info/training-profiles/training-profile/index.tsx
@@ -1,14 +1,44 @@
 import Layout from "@root/layouts";
 import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import Page from "@root/components/Page";
+import TrainingProfileTable from "@root/sections/carer-info/training-profile/TrainingProfile";
 
+// ----------------------------------------------------------------------
+// Constants
+const BREADCRUMBS = [
+  {
+    icon: <HomeIcon />,
+    name: "Carer Info",
+    href: "/carer-info",
+  },
+  {
+    name: "Trainings List",
+    href: "",
+  },
+];
+
+const PAGE_TITLE = "Training Profile";
 // ----------------------------------------------------------------------
 
 TrainingProfile.getLayout = function getLayout(page: any) {
-  return <Layout variant="dashboard">{page}</Layout>;
+  return (
+    <Layout
+      showTitleWithBreadcrumbs
+      breadcrumbs={BREADCRUMBS}
+      title={PAGE_TITLE}
+    >
+      {page}
+    </Layout>
+  );
 };
 
 // ----------------------------------------------------------------------
 
 export default function TrainingProfile() {
-  return <div>TrainingProfile</div>;
+  return (
+    <Page title={PAGE_TITLE}>
+      <TrainingProfileTable />
+    </Page>
+  )
 }
