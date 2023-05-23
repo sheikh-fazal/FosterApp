5757650f0daf21388c36be69abcdb7b9bb68a277
ali <ali.hassan@ceative.co.uk>
2023-05-23 12:09:14 +0500

    training evaluation form

---
+++ src/sections/training/manage-trainers/trainer-evaluation/index.tsx
@@ -0,0 +1,20 @@
+export const TableDummyData = [
+  {
+    trainingName: "Foster Carer Basic Training",
+    trainerName: "John Carlos",
+    trainingDate: "12.11.2022",
+    evaluationForm: "form",
+    evaluationProvideBy: "John Carlos",
+    evaluationDate: "24.04.2021",
+    approvedDate: "12.11.2021",
+  },
+  {
+    trainingName: "Foster Carer Basic Training",
+    trainerName: "John Carlos",
+    trainingDate: "12.11.2022",
+    evaluationForm: "form",
+    evaluationProvideBy: "John Carlos",
+    evaluationDate: "24.04.2021",
+    approvedDate: "12.11.2021",
+  },
+];
