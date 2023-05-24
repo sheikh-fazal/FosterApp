f340d34bde2e5ac09d84bf40b036c48d6c9ef714
Ayaz-khan <ayaz.khan@ceative.co.uk>
2023-05-24 08:43:41 +0500

    tables and data added to Home tab accordions. as per figma

--- src/sections/training/manage-trainers/training-sessions/training-sessions-home/event-history/useEventHistory.ts
+++ src/sections/training/manage-trainers/training-sessions/training-sessions-home/event-history/useEventHistory.ts
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { ONGOING_EVENT_TRAINING_SESSIONS_DATA } from '@root/dropdown-data/ongoingEventTrainingSessionsData'
 export const useEventHistory = () => {
+  const [openDelete, setOpenDelete] = useState(false);
+  const [data, setData] = useState(ONGOING_EVENT_TRAINING_SESSIONS_DATA)
   return {
-
+    data,
+    openDelete,
+    setOpenDelete
   }
 }
