4b46848f0aa1a1792e290c730f0e5261973c62a8
Ayaz-khan <ayaz.khan@ceative.co.uk>
2023-05-24 11:59:34 +0500

    first tab schedule has been added with a data table for full schedule data

--- src/sections/training/manage-trainers/training-sessions/training-sessions-schedule/TrainingSessionsSchedule.tsx
+++ src/sections/training/manage-trainers/training-sessions/training-sessions-schedule/TrainingSessionsSchedule.tsx
@@ -1,6 +1,7 @@
 import VericalTabs from "@root/components/VericalTabs";
 import React from "react";
 import { useTrainingSessionsSchedule } from "./useTrainingSessionsSchedule";
+import { ScheduleDataTable } from "./schedule-data-table";
 
 const TrainingSessionsSchedule = () => {
   const { scheduleDataMap } = useTrainingSessionsSchedule();
@@ -8,7 +9,7 @@ const TrainingSessionsSchedule = () => {
   return (
     <div>
       <VericalTabs tabsDataArray={scheduleDataMap}>
-        
+        <ScheduleDataTable />
       </VericalTabs>
     </div>
   );
