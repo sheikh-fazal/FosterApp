35cb4825c689ae5f813d04762d32b90dc736904f
Abdullah Sarwar Mustafai <abdullah.sarwar@ceative.co.uk>
2023-05-29 14:52:03 +0500

    #comment panel-dashboard fixes

--- src/sections/panel/panel-dashboard/panel-notifications/PanelNotifications.tsx
+++ src/sections/panel/panel-dashboard/panel-notifications/PanelNotifications.tsx
@@ -1,11 +1,11 @@
 import {
-    Box,
-    Button,
-    Card,
-    Checkbox,
-    FormControlLabel,
-    FormGroup,
-    Typography,
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import ExpandMore from "@mui/icons-material/ExpandMore";
@@ -14,72 +14,78 @@ import { useTheme } from "@emotion/react";
 import { NotificationData } from ".";
 
 export default function PanelNotifications() {
-    let [expand, setExpand] = useState(false);
-    const [notifications, setNotifications] = useState(NotificationData.slice(0, 3));
-    const handleExpand = () => {
-        setExpand(!expand);
-        !expand ? setNotifications(NotificationData) : setNotifications(NotificationData.slice(0, 3))
-    }
-    const theme: any = useTheme();
+  let [expand, setExpand] = useState(false);
+  const [notifications, setNotifications] = useState(
+    NotificationData.slice(0, 3)
+  );
+  const handleExpand = () => {
+    setExpand(!expand);
+    !expand
+      ? setNotifications(NotificationData)
+      : setNotifications(NotificationData.slice(0, 3));
+  };
+  const theme: any = useTheme();
 
-    return (
-        <Card
-            sx={{
-                py: 1.5,
-                px: 1,
-                boxShadow: "0px 0px 7px 3px rgba(14, 145, 140, 0.2)",
-                borderRadius: "10px",
-            }}
-        >
-            <Typography
-                variant="h6"
-                component="h6"
-                sx={{ color: theme.palette.primary.main, mb: 1 }}
-            >
-                Panel Notifications
-            </Typography>
-            <FormGroup >
-                <Box sx={styles.box}>
-                    {notifications.map((data: any, i: number) => (
-                        <FormControlLabel
-                            key={i}
-                            control={
-                                <Checkbox
-                                    name={data.name}
-                                    size="small"
-                                    onChange={(e) => {
-                                        console.log("aa", e.target.checked);
-                                    }}
-                                />
-                            }
-                            label={data.title}
-                        />
-                    ))}
-                </Box>
-            </FormGroup>
-            <Box
-                sx={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "flex-end",
-                }}
-            >
-                <Button
-                    onClick={handleExpand}
-                >
-                    {expand ? "less" : "more"}
-                    {expand ? <ExpandLess /> : <ExpandMore />}
-                </Button>
-            </Box>
-        </Card>
-    );
+  return (
+    <Card
+      sx={{
+        py: 1.5,
+        px: 1,
+        boxShadow: "0px 0px 7px 3px rgba(14, 145, 140, 0.2)",
+        borderRadius: "10px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        minHeight: "18rem",
+      }}
+    >
+      <Typography
+        variant="h6"
+        component="h6"
+        sx={{ color: theme.palette.primary.main, mb: 1 }}
+      >
+        Panel Notifications
+      </Typography>
+      <FormGroup>
+        <Box sx={styles.box}>
+          {notifications.map((data: any, i: number) => (
+            <FormControlLabel
+              key={i}
+              control={
+                <Checkbox
+                  name={data.name}
+                  size="small"
+                  onChange={(e) => {
+                    console.log("aa", e.target.checked);
+                  }}
+                />
+              }
+              label={data.title}
+            />
+          ))}
+        </Box>
+      </FormGroup>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button onClick={handleExpand}>
+          {expand ? "less" : "more"}
+          {expand ? <ExpandLess /> : <ExpandMore />}
+        </Button>
+      </Box>
+    </Card>
+  );
 }
 
 const styles = {
-    box: {
-        display: "flex",
-        flexDirection: "column",
-        maxHeight: "250px",
-        overflow: "scroll",
-    }
-}
+  box: {
+    display: "flex",
+    flexDirection: "column",
+    maxHeight: "250px",
+    overflow: "scroll",
+  },
+};
