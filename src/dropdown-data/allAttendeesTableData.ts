13f940d1348489e06c36df25e40da22417ce8ad3
Ayaz-khan <ayaz.khan@ceative.co.uk>
2023-05-29 12:39:53 +0500

    table data added to all horizontal tabs tables

---
+++ src/dropdown-data/allAttendeesTableData.ts
@@ -0,0 +1,11 @@
+export const All_ATTENDEES_DATA = [
+    {
+        date: "Hikaru Kubo-Kingsley",
+        trainingTopic: "Families for a Safe Home",
+        manager: "COO",
+        schedule: "Hikaru@gmail.com",
+        presenter: "(133) 565-5486",
+        attendees:"Yes",
+        budget: "Katina is subbing in for Russell, because",
+    },
+  ]
