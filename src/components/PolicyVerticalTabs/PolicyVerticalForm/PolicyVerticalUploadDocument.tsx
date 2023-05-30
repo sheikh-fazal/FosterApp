da52f0f3536d8013f56c5967baf600e86098ffb9
Muhammad Hassan <muhammadhassan@ceative.co.uk>
2023-05-30 14:11:44 +0500

    #comment policy code updated

--- src/components/PolicyVerticalTabs/PolicyVerticalForm/PolicyVerticalUploadDocument.tsx
+++ src/components/PolicyVerticalTabs/PolicyVerticalForm/PolicyVerticalUploadDocument.tsx
@@ -5,122 +5,114 @@ import TableHeader from "@root/components/TableHeader";
 import React, { useState } from "react";
 
 const PolicyVerticalUploadDocument = (props: any) => {
-  const { data, handleSubmit, isUploadBackBtn, addUploadDocument, onSubmitUploadDocument } = props;
-  const theme = useTheme();
+    const { data, handleSubmit, isUploadBackBtn, addUploadDocument, handleBackBtn, handleAction } = props;
+    const theme = useTheme();
 
-  // const handleSubmit = () => {
-  //     if(count === 1) {
-  //         navgiate.push('/policies-and-guidelines/organisational-policies/');
-  //     } else {
-  //         setIsUploadDocumentOpenModal(true);
-  //     }
-  //   };
+    const columns = [
+        {
+            id: "select",
+            header: ({ table, row }: any) => {
+                console.log(table.getSelectedRowModel().flatRows);
+                return (
+                    <Box>
+                        <Checkbox checked={table.getIsAllRowsSelected()} onChange={table.getToggleAllRowsSelectedHandler()} />
+                    </Box>
+                );
+            },
+            cell: ({ row, table }: any) => (
+                <Box>
+                    <Checkbox disabled={row?.original?.Assigned} checked={row?.original?.Assigned ? false : row.getIsSelected()} onChange={row.getToggleSelectedHandler()} />
+                </Box>
+            ),
+        },
+        {
+            accessorFn: (row: any) => row.no,
+            id: "Sr. No",
+            cell: (info: any) => info.getValue(),
+            header: () => <span>Sr. No</span>,
+            isSortable: true,
+        },
+        {
+            accessorFn: (row: any) => row.documentName,
+            id: "Title",
+            cell: (info: any) => info.getValue(),
+            header: () => <span>Document Name</span>,
+            isSortable: true,
+        },
+        {
+            accessorFn: (row: any) => row.documentType,
+            id: "Document Type",
+            cell: (info: any) => info.getValue(),
+            header: () => <span>Document Type</span>,
+            isSortable: true,
+        },
+        {
+            accessorFn: (row: any) => row.documentDate,
+            id: "Document Date",
+            cell: (info: any) => info.getValue(),
+            header: () => <span>Document Date</span>,
+            isSortable: true,
+        },
+        {
+            accessorFn: (row: any) => row.personUploaded,
+            id: "Person Uploaded",
+            cell: (info: any) => info.getValue(),
+            header: () => <span>Person Uploaded</span>,
+            isSortable: true,
+        },
+        {
+            accessorFn: (row: any) => row.password,
+            id: "Password",
+            cell: (info: any) => info.getValue(),
+            header: () => <span>Password</span>,
+            isSortable: true,
+        },
+        {
+            id: "actions",
+            cell: (info: any) => (
+                <Box display={"flex"} gap={0.5}>
+                    {["download", "delete", "view"].map((action) => (
+                        <span key={action} style={{ flexShrink: 0 }}>
+                            <TableAction type={action} onClicked={() => handleAction(action, info.row.original)} />
+                        </span>
+                    ))}
+                </Box>
+            ),
+            header: () => <span>actions</span>,
+        },
+    ];
 
-  const columns = [
-    {
-      id: "select",
-      header: ({ table, row }: any) => {
-        console.log(table.getSelectedRowModel().flatRows);
-        return (
-          <Box>
-            <Checkbox checked={table.getIsAllRowsSelected()} onChange={table.getToggleAllRowsSelectedHandler()} />
-          </Box>
-        );
-      },
-      cell: ({ row, table }: any) => (
-        <Box>
-          <Checkbox disabled={row?.original?.Assigned} checked={row?.original?.Assigned ? false : row.getIsSelected()} onChange={row.getToggleSelectedHandler()} />
-        </Box>
-      ),
-    },
-    {
-      accessorFn: (row: any) => row.no,
-      id: "Sr. No",
-      cell: (info: any) => info.getValue(),
-      header: () => <span>Sr. No</span>,
-      isSortable: true,
-    },
-    {
-      accessorFn: (row: any) => row.documentName,
-      id: "Title",
-      cell: (info: any) => info.getValue(),
-      header: () => <span>Document Name</span>,
-      isSortable: true,
-    },
-    {
-      accessorFn: (row: any) => row.documentType,
-      id: "Document Type",
-      cell: (info: any) => info.getValue(),
-      header: () => <span>Document Type</span>,
-      isSortable: true,
-    },
-    {
-      accessorFn: (row: any) => row.documentDate,
-      id: "Document Date",
-      cell: (info: any) => info.getValue(),
-      header: () => <span>Document Date</span>,
-      isSortable: true,
-    },
-    {
-      accessorFn: (row: any) => row.personUploaded,
-      id: "Person Uploaded",
-      cell: (info: any) => info.getValue(),
-      header: () => <span>Person Uploaded</span>,
-      isSortable: true,
-    },
-    {
-      accessorFn: (row: any) => row.password,
-      id: "Password",
-      cell: (info: any) => info.getValue(),
-      header: () => <span>Password</span>,
-      isSortable: true,
-    },
-    {
-      id: "actions",
-      cell: (info: any) => (
-        <Box display={"flex"} gap={0.5}>
-          {["download", "delete", "view"].map((action) => (
-            <span key={action} style={{ flexShrink: 0 }}>
-              <TableAction type={action} onClicked={() => alert(action)} />
-            </span>
-          ))}
-        </Box>
-      ),
-      header: () => <span>actions</span>,
-    },
-  ];
-
-  return (
-    <>
-      <TableHeader title="Uploaded Documents" showAddBtn onAdd={addUploadDocument} />
-      <CustomTable
-        data={data}
-        columns={columns}
-        isLoading={false}
-        isFetching={false}
-        isError={false}
-        isSuccess={true}
-        currentPage={1}
-        onPageChange={(data: any) => {
-          console.log("Current page data: ", data);
-        }}
-        onSortByChange={(data: any) => {
-          console.log("Sort by: ", data);
-        }}
-        rootSX={{ my: theme.spacing(2) }}
-      />
-      <Box>
-        <Button type="submit" variant="contained" sx={{ mr: 2 }} onClick={handleSubmit}>
-          Submit
-        </Button>
-        {isUploadBackBtn && (
-          <Button sx={{ backgroundColor: "#F6830F", "&:hover": { backgroundColor: "#F6830F" } }} type="button" variant="contained">
-            Back
-          </Button>
-        )}
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <TableHeader title="Uploaded Documents" showAddBtn onAdd={addUploadDocument} />
+            <CustomTable
+                data={data}
+                columns={columns}
+                isLoading={false}
+                isFetching={false}
+                isError={false}
+                isSuccess={true}
+                currentPage={1}
+                onPageChange={(data: any) => {
+                    console.log("Current page data: ", data);
+                }}
+                onSortByChange={(data: any) => {
+                    console.log("Sort by: ", data);
+                }}
+                rootSX={{ my: theme.spacing(2) }}
+            />
+            <Box>
+                <Button type="submit" variant="contained" sx={{ mr: 2 }} onClick={handleSubmit}>
+                    Submit
+                </Button>
+                {isUploadBackBtn && (
+                    <Button sx={{ backgroundColor: "#F6830F", "&:hover": { backgroundColor: "#F6830F" } }} type="button" variant="contained" onClick={handleBackBtn}>
+                        Back
+                    </Button>
+                )}
+            </Box>
+        </>
+    );
 };
 
 export default PolicyVerticalUploadDocument;
--- src/sections/policies-and-guidelines/organisational-policies/organisational-policies-tabs/OrganisationalPoliciesTabs.tsx
+++ src/sections/policies-and-guidelines/organisational-policies/organisational-policies-tabs/OrganisationalPoliciesTabs.tsx
@@ -1,38 +1,26 @@
 import PolicyVerticalAddNew from '@root/components/PolicyVerticalTabs/PolicyVerticalForm/PolicyVerticalAddNew/PolicyVerticalAddNew'
-import { usePolicyVerticalAddNew } from '@root/components/PolicyVerticalTabs/PolicyVerticalForm/PolicyVerticalAddNew/usePolicyVerticalAddNew'
 import PolicyVerticalUploadDocument from '@root/components/PolicyVerticalTabs/PolicyVerticalForm/PolicyVerticalUploadDocument'
 import CustomHorizaontalTab from '@root/components/customTabs'
-import React, { useState } from 'react'
+import React from 'react'
 import { uploadDocumentData } from '.'
 import UploadDocumentModal from '@root/components/modal/UploadDocumentModal/UploadDocumentModal'
-import { useRouter } from 'next/router'
+import DeleteModel from '@root/components/modal/DeleteModel'
+import { useOrganisationalPoliciesTabs } from './useOrganisationalPoliciesTabs'
 
 const OrganisationalPoliciesTabs = () => {
-    const { currentTab, setCurrentTab, handleNextTab } = usePolicyVerticalAddNew();
-    const [isUploadDocumentOpenModal, setIsUploadDocumentOpenModal] = useState(false);
-    const [count, setCount] = useState(0);
-    const navgiate = useRouter();
-
-    const handleSubmit = (data: any) => {
-        setCount(0)
-        handleNextTab();
-    }
-
-    const handleUploadedSubmit = () => {
-        if(count === 1) {
-            navgiate.push('/policies-and-guidelines/organisational-policies/');
-        } else {
-            setIsUploadDocumentOpenModal(true);
-        }
-      };
+   const { currentTab, setCurrentTab, handlePreviousTab, isUploadDocumentOpenModal, isDeleteOpenModal, setCount, handleSubmit, handleUploadedSubmit, handleAction, setIsUploadDocumentOpenModal,setIsDeleteOpenModal, count } = useOrganisationalPoliciesTabs();
 
     return (
         <>
-            <CustomHorizaontalTab tabsArray={["Add New Policy", "Upload document"]} currentTab={currentTab} setCurrentTab={setCurrentTab} >
+            <CustomHorizaontalTab tabsArray={["Add New Policy", "Upload document"]} currentTab={currentTab} setCurrentTab={setCurrentTab} isDisabled={true}>
                 <PolicyVerticalAddNew onSubmit={handleSubmit} handleAddNewBack={'/policies-and-guidelines/organisational-policies/'} />
-                <PolicyVerticalUploadDocument data={uploadDocumentData} addUploadDocument={() => setIsUploadDocumentOpenModal(true)} handleSubmit={handleUploadedSubmit} />
+
+                <PolicyVerticalUploadDocument data={uploadDocumentData} addUploadDocument={() => setIsUploadDocumentOpenModal(true)} handleSubmit={handleUploadedSubmit} isUploadBackBtn={count === 1 && true} handleBackBtn={() => handlePreviousTab()} handleAction={handleAction} />
             </CustomHorizaontalTab>
-            <UploadDocumentModal open={isUploadDocumentOpenModal} handleClose={() => setIsUploadDocumentOpenModal(false)} isHideSubmitButton={true} onSubmit={() => setIsUploadDocumentOpenModal(false)} />
+
+            <UploadDocumentModal open={isUploadDocumentOpenModal} handleClose={() => setIsUploadDocumentOpenModal(false)} isHideSubmitButton={true} onSubmit={() => { setIsUploadDocumentOpenModal(false); setCount(1) }} />
+
+            <DeleteModel open={isDeleteOpenModal} handleClose={() => setIsDeleteOpenModal(false)} />
         </>
     )
 }
