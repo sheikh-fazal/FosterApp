529d6465c2c146f6141f4606314071f20d3b5939
shehroz bhatti <shehrozsaeed.bhatti@ceative.co.uk>
2023-05-30 15:58:53 +0500

    privacy and policy handbook

--- src/sections/policies-and-guidelines/handbooks-policies/handbooks-policies-tabs/HandbookPoliciesTabs.tsx
+++ src/sections/policies-and-guidelines/handbooks-policies/handbooks-policies-tabs/HandbookPoliciesTabs.tsx
@@ -3,15 +3,23 @@ import PolicyVerticalUploadDocument from '@root/components/PolicyVerticalTabs/Po
 import CustomHorizaontalTab from '@root/components/customTabs';
 import { uploadDocumentData } from '.'
 import React from 'react'
-import { usePolicyVerticalAddNew } from '@root/components/PolicyVerticalTabs/PolicyVerticalForm/PolicyVerticalAddNew/usePolicyVerticalAddNew';
+// import { usePolicyVerticalAddNew } from '@root/components/PolicyVerticalTabs/PolicyVerticalForm/PolicyVerticalAddNew/usePolicyVerticalAddNew';
+import UploadDocumentModal from '@root/components/modal/UploadDocumentModal/UploadDocumentModal';
+import DeleteModel from '@root/components/modal/DeleteModel';
+import { useHandbookPoliciesTabs } from './useHandbookPoliciesTabs';
 
 const HandbookPoliciesTabs = () => {
-    const { currentTab, setCurrentTab } = usePolicyVerticalAddNew();
+    const { currentTab, setCurrentTab, handlePreviousTab, isUploadDocumentOpenModal, isDeleteOpenModal, setCount, handleSubmit, handleUploadedSubmit, handleAction, setIsUploadDocumentOpenModal, setIsDeleteOpenModal, count } = useHandbookPoliciesTabs();
     return (
-        <CustomHorizaontalTab tabsArray={["Add New Handbook", "Upload document"]} currentTab={currentTab} setCurrentTab={setCurrentTab}>
-            <PolicyVerticalAddNew />
-            <PolicyVerticalUploadDocument data={uploadDocumentData} />
-        </CustomHorizaontalTab>
+        <>
+            <CustomHorizaontalTab tabsArray={["Add New Handbook", "Upload document"]} currentTab={currentTab} setCurrentTab={setCurrentTab} isDisabled={true}>
+                <PolicyVerticalAddNew onSubmit={handleSubmit} handleAddNewBack={'/policies-and-guidelines/organisational-policies/'} />
+                <PolicyVerticalUploadDocument data={uploadDocumentData} addUploadDocument={() => setIsUploadDocumentOpenModal(true)} handleSubmit={handleUploadedSubmit} isUploadBackBtn={count === 1 && true} handleBackBtn={() => handlePreviousTab()} handleAction={handleAction} />
+            </CustomHorizaontalTab>
+
+            <UploadDocumentModal open={isUploadDocumentOpenModal} handleClose={() => setIsUploadDocumentOpenModal(false)} isHideSubmitButton={true} onSubmit={() => { setIsUploadDocumentOpenModal(false); setCount(1) }} />
+            <DeleteModel open={isDeleteOpenModal} handleClose={() => setIsDeleteOpenModal(false)} />
+        </>
     )
 }
 
