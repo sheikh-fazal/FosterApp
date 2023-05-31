01aa94d9d7b182f02bc327ebf7636a568c21f0ae
Valeed anjum <valeed.anjum@ceative.co.uk>
2023-05-31 22:07:43 +0500

    Immunisation update api

--- src/sections/edit-profile/medical-history/immunisation/Immunisation.tsx
+++ src/sections/edit-profile/medical-history/immunisation/Immunisation.tsx
@@ -36,6 +36,10 @@ import {
   useDeleteImmunisationInfoDocuMutation,
 } from "@root/services/update-profile/medical-history/medicalHistory";
 import MultipleFileUploader from "../../file-uploaders/multifile-uploader/MultipleFileUploader";
+import FormSkeleton from "../../render-form/FormSkeleton";
+import IsFetching from "@root/components/loaders/IsFetching";
+import { displayErrorMessage, displaySuccessMessage } from "../../util/Util";
+import { enqueueSnackbar } from "notistack";
 
 const Immunisation: FC<any> = ({ activateNextForm }) => {
   const theme: any = useTheme();
@@ -49,12 +53,23 @@ const Immunisation: FC<any> = ({ activateNextForm }) => {
 
   const [getImmunisationInfo] = useLazyGetImmunisationInfoQuery();
   const [updateImmunisationInfo] = useUpdateImmunisationInfoMutation();
-  const [deleteImmunisationInfoDocu] = useDeleteImmunisationInfoDocuMutation();
+  // const [deleteImmunisationInfoDocu] = useDeleteImmunisationInfoDocuMutation();
 
   const methods: any = useForm({
-    // mode: "onTouched",
     resolver: yupResolver(FormSchema),
-    defaultValues,
+    defaultValues: async () => {
+      const { data, isError } = await getImmunisationInfo(null, false);
+      console.log({ data });
+      setAvailableFiles(data?.data?.certificate);
+      setIsLoading(false);
+      if (isError) {
+        return defaultValues;
+      }
+      return {
+        ...data?.data,
+        dob: new Date(),
+      };
+    },
   });
 
   const {
@@ -78,6 +93,18 @@ const Immunisation: FC<any> = ({ activateNextForm }) => {
 
   const onSubmit = async (data: any) => {
     console.log({ data });
+    const formData = new FormData();
+    documents.forEach((doc) => formData.append("certificate", doc));
+    for (var key in data) {
+      formData.append(key, data[key]);
+    }
+    try {
+      const data = await updateImmunisationInfo(formData);
+      displaySuccessMessage(data, enqueueSnackbar);
+      // activateNextForm();
+    } catch (error: any) {
+      displayErrorMessage(error, enqueueSnackbar);
+    }
     // activateNextForm();
   };
 
@@ -94,56 +121,17 @@ const Immunisation: FC<any> = ({ activateNextForm }) => {
     //   return false;
     // }
   };
-
+  if (isLoading) return <FormSkeleton />;
   return (
-    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Grid container justifyContent="center">
-        <Grid container item xs={12}>
-          <Grid item sm={12} container>
-            <Grid item sm={12} container direction="column">
-              <Grid item sx={{ padding: "0.5em" }}>
-                {/* Heading And Icon Con  */}
-                <Grid item container>
-                  <Grid item>
-                    <Typography
-                      sx={{
-                        fontWeight: 600,
-                        color: theme.palette.primary.main,
-                      }}
-                    >
-                      Immunisation
-                    </Typography>
-                  </Grid>
-                </Grid>
-                {/* certificate issue And expiry date  */}
-                <Grid container>
-                  {/* Dynamically Generated Fields  */}
-                  {fieldsInfo.map((item: any, index: number) => {
-                    return (
-                      <Fragment key={index}>
-                        {/* if there is only one field that is accoupies whole width   */}
-                        {item.length === 1 && (
-                          <FullWidthFormField
-                            item={item}
-                            isSubmitting={isSubmitting}
-                            disabled={disabled}
-                          />
-                        )}
-                        {/* if there are two fields with 50% 50% width   */}
-                        {item.length === 2 && (
-                          <HalfWidthFormField
-                            item={item}
-                            isSubmitting={isSubmitting}
-                            disabled={disabled}
-                          />
-                        )}
-                      </Fragment>
-                    );
-                  })}
-                  {/* A Custom Field On Full Width  */}
-                </Grid>
-                {/* Upload Certificates Heading And Icons  */}
-                <Grid item sm={12}>
+    <>
+      {isSubmitting && <IsFetching isFetching />}
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Grid container justifyContent="center">
+          <Grid container item xs={12}>
+            <Grid item sm={12} container>
+              <Grid item sm={12} container direction="column">
+                <Grid item sx={{ padding: "0.5em" }}>
+                  {/* Heading And Icon Con  */}
                   <Grid item container>
                     <Grid item>
                       <Typography
@@ -152,44 +140,86 @@ const Immunisation: FC<any> = ({ activateNextForm }) => {
                           color: theme.palette.primary.main,
                         }}
                       >
-                        Upload Certificates
+                        Immunisation
                       </Typography>
                     </Grid>
                   </Grid>
-                </Grid>
-                {/* File Uploader  */}
-                <Grid item sm={12} container direction="column">
-                  <Grid item sx={{ padding: "0.5em" }}>
-                    <MultipleFileUploader
-                      availableFiles={availableFiles}
-                      setAvailableFiles={setAvailableFiles}
-                      setDocuments={setDocuments}
-                      deleteDocument={deleteDocument}
-                    />
+                  {/* certificate issue And expiry date  */}
+                  <Grid container>
+                    {/* Dynamically Generated Fields  */}
+                    {fieldsInfo.map((item: any, index: number) => {
+                      return (
+                        <Fragment key={index}>
+                          {/* if there is only one field that is accoupies whole width   */}
+                          {item.length === 1 && (
+                            <FullWidthFormField
+                              item={item}
+                              isSubmitting={isSubmitting}
+                              disabled={disabled}
+                            />
+                          )}
+                          {/* if there are two fields with 50% 50% width   */}
+                          {item.length === 2 && (
+                            <HalfWidthFormField
+                              item={item}
+                              isSubmitting={isSubmitting}
+                              disabled={disabled}
+                            />
+                          )}
+                        </Fragment>
+                      );
+                    })}
+                    {/* A Custom Field On Full Width  */}
+                  </Grid>
+                  {/* Upload Certificates Heading And Icons  */}
+                  <Grid item sm={12}>
+                    <Grid item container>
+                      <Grid item>
+                        <Typography
+                          sx={{
+                            fontWeight: 600,
+                            color: theme.palette.primary.main,
+                          }}
+                        >
+                          Upload Certificates
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  {/* File Uploader  */}
+                  <Grid item sm={12} container direction="column">
+                    <Grid item sx={{ padding: "0.5em" }}>
+                      <MultipleFileUploader
+                        availableFiles={availableFiles}
+                        setAvailableFiles={setAvailableFiles}
+                        setDocuments={setDocuments}
+                        deleteDocument={deleteDocument}
+                      />
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          {!disabled && (
-            <Grid item sm={12} container direction="column">
-              <Grid item container sx={{ padding: "0.5em" }} spacing={1}>
-                <Grid item>
-                  <Button variant="contained" type="submit">
-                    Save
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="contained" type="submit">
-                    Continue
-                  </Button>
+            {!disabled && (
+              <Grid item sm={12} container direction="column">
+                <Grid item container sx={{ padding: "0.5em" }} spacing={1}>
+                  <Grid item>
+                    <Button variant="contained" type="submit">
+                      Save
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="contained" type="submit">
+                      Continue
+                    </Button>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          )}
+            )}
+          </Grid>
         </Grid>
-      </Grid>
-    </FormProvider>
+      </FormProvider>
+    </>
   );
 };
 
