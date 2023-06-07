ed195cc00c55491a24378300a9177f0ae27d70ec
Abdullah Sarwar Mustafai <abdullah.sarwar@ceative.co.uk>
2023-06-07 11:10:17 +0500

    #comment organisational policy extra fragment

--- src/sections/policies-and-guidelines/organisational-policies/OrganisationalPolicies.tsx
+++ src/sections/policies-and-guidelines/organisational-policies/OrganisationalPolicies.tsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { OrganisationalPoliciesData } from ".";
 import PolicyVerticalTable from "@root/components/policy-vertical-tabs/PolicyVerticalTable";
 
 const OrganisationalPolicies = () => {
+  return (
+    <PolicyVerticalTable
+      data={OrganisationalPoliciesData}
+      addNewTabNavigation={
+        "/policies-and-guidelines/organisational-policies/organisational-policies-form"
+      }
+      addNewFormNavigation={
+        "/policies-and-guidelines/organisational-policies/organisational-policies-form"
+      }
+    />
+  );
+};
 
-    return (
-        <>
-            <PolicyVerticalTable data={OrganisationalPoliciesData} addNewTabNavigation={'/policies-and-guidelines/organisational-policies/organisational-policies-form'} addNewFormNavigation={'/policies-and-guidelines/organisational-policies/organisational-policies-form'}  />
-        </>
-    )
-}
-
-export default OrganisationalPolicies; 
+export default OrganisationalPolicies;
