9c1cd82cf8941b5ffd3dca3e0c94a78e90471a69
valeed <valeed.anjum@ceative.co.uk>
2023-06-08 19:14:12 +0500

    User Profile

--- src/pages/foster-child/other-information/safe-care-policy/index.tsx
+++ src/pages/foster-child/other-information/safe-care-policy/index.tsx
@@ -1,5 +1,41 @@
-import React from "react";
+import Page from "@root/components/Page";
+import Layout from "@root/layouts";
+//  @mui icons
+import HomeIcon from "@mui/icons-material/Home";
 
-export default function SafeCarePolicy() {
-  return <div>SafeCarePolicy</div>;
+// ----------------------------------------------------------------------
+// Constants
+const BREADCRUMBS = [
+  {
+    icon: <HomeIcon />,
+    name: "/ Child Info",
+    href: "/foster-child",
+  },
+  {
+    name: "Safe Care Policies List",
+    href: "/foster-child/ABC",
+  },
+];
+
+const PAGE_TITLE = "Safe Care Policy";
+
+// ----------------------------------------------------------------------
+
+InitialHomeVisit.getLayout = function getLayout(page: any) {
+  return (
+    <Layout
+      showTitleWithBreadcrumbs
+      breadcrumbs={BREADCRUMBS}
+      title={PAGE_TITLE}
+      variant="dashboard"
+    >
+      {page}
+    </Layout>
+  );
+};
+
+// ----------------------------------------------------------------------
+
+export default function InitialHomeVisit() {
+  return <Page title={PAGE_TITLE}>Safe Care</Page>;
 }
