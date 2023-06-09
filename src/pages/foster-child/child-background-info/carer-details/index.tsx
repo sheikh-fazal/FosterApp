4152569edf1d670d73f72eca4dddf426cfcd638e
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2023-06-09 20:40:45 +0500

    Carer Details Header UI

--- src/pages/foster-child/child-background-info/carer-details/index.tsx
+++ src/pages/foster-child/child-background-info/carer-details/index.tsx
@@ -1,5 +1,69 @@
 import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import Layout from "@root/layouts";
+import Page from "@root/components/Page";
+import { Card, Stack } from "@mui/material";
+import { CarerDetailsHeader } from "@root/sections/foster-child/child-background-info/carer-details/carer-details-header";
+import { CarerDetailsForm } from "@root/sections/foster-child/child-background-info/carer-details";
+import HorizaontalTabs from "@root/components/HorizaontalTabs";
+
+// ----------------------------------------------------------------------
+
+const PAGE_TITLE = "Carer Details";
+
+// ----------------------------------------------------------------------
+
+const BREADCRUMBS = [
+  {
+    icon: <HomeIcon />,
+    name: " ",
+    href: "/",
+  },
+  {
+    name: "Child Info",
+    href: "/foster-child",
+  },
+  {
+    name: PAGE_TITLE,
+    href: "",
+  },
+];
+
+// ----------------------------------------------------------------------
+
+CarerDetails.getLayout = function getLayout(page: any) {
+  return (
+    <Layout
+      showTitleWithBreadcrumbs
+      breadcrumbs={BREADCRUMBS}
+      title={PAGE_TITLE}
+    >
+      {page}
+    </Layout>
+  );
+};
 
 export default function CarerDetails() {
-  return <div>CarerDetails</div>;
+  return (
+    <Page title={PAGE_TITLE}>
+      <Stack direction={"column"} spacing={2}>
+        <CarerDetailsHeader />
+        <HorizaontalTabs
+          tabsDataArray={[
+            "Primary Carer",
+            "Secondary Carer",
+            "Backup Carer",
+            "Respite Carer",
+            "Swap Carer",
+          ]}
+        >
+          <CarerDetailsForm disabled />
+          <CarerDetailsForm disabled />
+          <CarerDetailsForm disabled />
+          <CarerDetailsForm disabled />
+          <CarerDetailsForm disabled />
+        </HorizaontalTabs>
+      </Stack>
+    </Page>
+  );
 }
---
+++ src/sections/foster-child/child-background-info/carer-details/CarerDetailsForm.tsx
@@ -0,0 +1,82 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Grid } from "@mui/material";
+import { FormProvider } from "@root/components/hook-form";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { CarerDetailsFormData, defaultValues, validationSchema } from "./";
+import { LoadingButton } from "@mui/lab";
+import Link from "next/link";
+
+export default function CarerDetailsForm({
+  disabled,
+  onSubmitHandler,
+  initialValueProps = defaultValues,
+  message,
+  isError,
+  isSuccess,
+}: any) {
+  const methods: any = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues: initialValueProps,
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+    // try {
+    //   const res: any = await onSubmitHandler(data).unwrap();
+    //   enqueueSnackbar(res?.message ?? `Carer Family ${message} Successfully!`, {
+    //     variant: "success",
+    //   });
+    //   router.push("/carer-info/personal-info/carer-family-support-network");
+    // } catch (error: any) {
+    //   const errMsg = error?.data?.message;
+    //   enqueueSnackbar(errMsg ?? "Something Went Wrong!", { variant: "error" });
+    // }
+  };
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={4}>
+        {CarerDetailsFormData?.map((item: any) => (
+          <Grid item xs={12} md={item?.md} key={item?.id}>
+            <item.component
+              disabled={disabled}
+              {...item.componentProps}
+              size={"small"}
+            >
+              {item?.componentProps?.select
+                ? item?.options?.map((option: any) => (
+                    <option key={option?.value} value={option?.value}>
+                      {option?.label}
+                    </option>
+                  ))
+                : null}
+            </item.component>
+          </Grid>
+        ))}
+        {!disabled && (
+          <Grid item xs={12}>
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              sx={{ mr: 2 }}
+              loading={isSubmitting}
+              color={isError ? "error" : isSuccess ? "success" : "primary"}
+            >
+              {isError ? "Try Again!" : isSuccess ? "Success" : "Submit"}
+            </LoadingButton>
+            <Link href={"#"} style={{ textDecoration: "none" }}>
+              <Button type="button" variant="contained">
+                Back
+              </Button>
+            </Link>
+          </Grid>
+        )}
+      </Grid>
+    </FormProvider>
+  );
+}
