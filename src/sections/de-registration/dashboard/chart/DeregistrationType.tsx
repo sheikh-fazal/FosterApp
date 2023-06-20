d5a07a2ffdf4d3418732f092c4f2ee6e3cce501b
shehroz bhatti <shehrozsaeed.bhatti@ceative.co.uk>
2023-06-20 15:57:11 +0500

    worked on deregister dashboard

---
+++ src/sections/de-registration/dashboard/chart/DeregistrationType.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import dynamic from "next/dynamic";
+import { ApexOptions } from "apexcharts";
+import { Box, Grid, Typography } from "@mui/material";
+import { pieChartTitles } from ".";
+
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+    ssr: false,
+});
+
+export const DeregistrationType = () => {
+    let series: number[] = [20, 40, 40,];
+    let options: ApexOptions = {
+        chart: {
+            type: "pie",
+        },
+        stroke: {
+            width: 1,
+        },
+        dataLabels: { enabled: false },
+        labels: ["Uncontested Resignation", "Termination with Allegation", "Termination with Allegation",],
+        colors: ["#E07601", "#849399", "#5BA316",],
+        legend: {
+            show: false,
+        },
+    };
+
+    return (
+        <Grid sx={{ px: { lg: 0, sm: 2, xs: 1 } }} container spacing={2}>
+            <Grid item lg={6} md={6} xs={12}>
+                <Box>
+                    <ReactApexChart options={options} series={series} type="pie" height={300} />
+                </Box>
+            </Grid>
+            <Grid item lg={6} md={6} xs={12}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "4px",
+                        marginTop: "10px",
+                    }}
+                >
+                    <Grid container spacing={2} sx={{ flexDirection: "column" }}>
+                        {pieChartTitles?.map((item, index) => (
+                            <Grid key={index} item lg={6} sx={{ flexDirection: "column" }}>
+                                <Box key={item?.color} sx={{ mt: 0.5 }}>
+                                    <Typography sx={{ whiteSpace: 'nowrap', mb: "6px" }}>{item?.title}</Typography>
+                                    <Box sx={{ display: "flex", gap: "6px", alignItems: "center" }}>
+                                        <Box
+                                            sx={{
+                                                width: "31px",
+                                                height: "28px",
+                                                background: item?.color,
+                                                borderRadius: "4px",
+                                            }}
+                                        ></Box>
+                                        <Typography variant="subtitle2" ml={"12px"}>{`${item?.percentage}%`}</Typography>
+                                    </Box>
+                                </Box>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+            </Grid>
+        </Grid>
+    );
+}
---
+++ src/sections/de-registration/dashboard/deregistration-management-table/DeregistrationManagementTable.tsx
@@ -0,0 +1,49 @@
+import CustomTable from '@root/components/Table/CustomTable'
+import TableHeader from '@root/components/TableHeader'
+import { AddChildModal } from '@root/sections/local-authority/view-children-list/add-child-modal'
+import useViewChildrenList from '@root/sections/local-authority/view-children-list/useViewChildrenList'
+import React, { useState } from 'react'
+
+const DeregistrationManagementTable = () => {
+    const { TableData, tableHeaderRefTwo, router, theme, SELECT_FILTERS, columns } = useViewChildrenList()
+    const [modalType, setModalType] = useState({ value: "", type: "" })
+
+    return (
+        <>
+            <TableHeader
+                ref={tableHeaderRefTwo}
+                title="Carer List"
+                showAddBtn
+                showSelectFilters={true}
+                selectFilters={SELECT_FILTERS}
+                onAdd={() => setModalType({ ...modalType, value: "", type: "add" })}
+            // onChanged={(data: any) => {
+            //     console.log("Updated params: ", data);
+            // }}
+            />
+            {/* table body start here */}
+            <CustomTable
+                data={TableData}
+                columns={columns}
+                isLoading={false}
+                isFetching={false}
+                isError={false}
+                isSuccess={true}
+                isPagination={false}
+                // count={Math.ceil(data?.data?.meta?.total / limit)}
+                currentPage={1}
+                onPageChange={(data: any) => {
+                    console.log("Current page data: ", data);
+                }}
+                onSortByChange={(data: any) => {
+                    console.log("Sort by: ", data);
+                }}
+                rootSX={{ my: theme.spacing(2) }}
+            />
+            <AddChildModal open={modalType.type} handleClose={() => { setModalType({ type: "", value: "" }); }} />
+        </>
+
+    )
+}
+
+export default DeregistrationManagementTable
