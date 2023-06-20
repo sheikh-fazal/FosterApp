1d9a1ca981c40a7803872501862c3c04e41dd7c5
valeed <valeed.anjum@ceative.co.uk>
2023-06-20 20:49:41 +0500

    Garbage Removed From The About The Candidate

--- src/sections/edit-profile/about-candidate/address-details/AddressDetails.tsx
+++ src/sections/edit-profile/about-candidate/address-details/AddressDetails.tsx
@@ -1,12 +1,10 @@
 import { Grid } from "@mui/material";
-import EditProfileHeader from "../../edit-profile-header/EditProfileHeader";
 import PersonalDetailsForm from "./addressDetailsForm/AddressDetailsForm";
 
 const AddressDetails = () => {
   return (
     <Grid container justifyContent="center">
       <Grid item sm={11}>
-        {/* <EditProfileHeader /> */}
         <PersonalDetailsForm />
       </Grid>
     </Grid>
