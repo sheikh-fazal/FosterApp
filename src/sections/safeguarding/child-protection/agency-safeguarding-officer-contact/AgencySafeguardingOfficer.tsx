4728c96d4be3eb03a601c220009fee5f68cd9cf7
khalid <khalid.mehmood@ceative.co.uk>
2023-06-21 15:49:55 +0500

    advocacy fixes

--- src/sections/safeguarding/child-protection/agency-safeguarding-officer-contact/AgencySafeguardingOfficer.tsx
+++ src/sections/safeguarding/child-protection/agency-safeguarding-officer-contact/AgencySafeguardingOfficer.tsx
@@ -19,7 +19,7 @@ const AgencySafeguardingOfficer = ({ defaultValue = 2 }: any) => {
     <>
       <Card sx={styles.card}>
         <TableHeader title={"Contact Directory"} ref={tableHeaderRef} searchKey="search" showAddBtn onAdd={handleEditOpen} />
-        <HorizaontalTabs tabsDataArray={tabsArr} setActiveTab={setActiveTab} defaultValue={defaultValue}>
+        <HorizaontalTabs tabsDataArray={tabsArr} setActiveTab={setActiveTab} defaultValue={defaultValue} disableBoxShadow>
           {contactDirectoryTabs.map((item: any, index: number) => (
             <Fragment key={index}>
               <ContactDirectoryTable data={filteredData} contactInfoModal={isEditModal} setContactInfoModal={handleEditOpen} />
