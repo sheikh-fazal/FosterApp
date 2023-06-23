dc5bd27394deae3131bbbccd32d5f8be049d142c
Abdullah Sarwar Mustafai <abdullah.sarwar@ceative.co.uk>
2023-06-23 12:45:34 +0500

    #comment add key in compliance

--- src/sections/compliance-management/ComplianceTabs.tsx
+++ src/sections/compliance-management/ComplianceTabs.tsx
@@ -4,16 +4,18 @@ import HorizaontalTabs from "@root/components/HorizaontalTabs";
 import Image from "next/image";
 import { useComplianceManagement } from "./useComplianceMangement";
 import CategoryModal from "./Modals/category-modal/CategoryModal";
-import AddIcon from '../../assets/svg/compliance-management/addIcon.svg';
-import PdfIcon from '../../assets/svg/compliance-management/pdfIcon.svg';
+import AddIcon from "../../assets/svg/compliance-management/addIcon.svg";
+import PdfIcon from "../../assets/svg/compliance-management/pdfIcon.svg";
 import PdfViewModal from "@root/components/modal/PdfViewModal/PdfViewModal";
 
 interface IVERTICALTABSPROPS {
   tabsDataArray: Array<Object>;
 }
 
-export default function ComplianceTabs({ tabsDataArray, ...other }: IVERTICALTABSPROPS) {
-
+export default function ComplianceTabs({
+  tabsDataArray,
+  ...other
+}: IVERTICALTABSPROPS) {
   const {
     currentTab,
     categoryModal,
@@ -43,7 +45,7 @@ export default function ComplianceTabs({ tabsDataArray, ...other }: IVERTICALTAB
             {tabsDataArray?.map((item: any) => (
               <Tab
                 key={item?.index}
-                sx={{ overflow: 'hidden' }}
+                sx={{ overflow: "hidden" }}
                 label={
                   <TitleWithIcon
                     currentTab={currentTab}
@@ -56,11 +58,11 @@ export default function ComplianceTabs({ tabsDataArray, ...other }: IVERTICALTAB
               />
             ))}
           </Tabs>
-          <Box sx={{ mt: 1, cursor: 'pointer' }}>
+          <Box sx={{ mt: 1, cursor: "pointer" }}>
             <TitleWithIcon
-              title={'Add More'}
+              title={"Add More"}
               icon={AddIcon}
-              color={'#F6830F'}
+              color={"#F6830F"}
               onClick={handleCategoryModal}
             />
           </Box>
@@ -83,74 +85,106 @@ export default function ComplianceTabs({ tabsDataArray, ...other }: IVERTICALTAB
                   onClick={() => handleSubCategory(item)}
                 />
                 <HorizaontalTabs
-                  tabsDataArray={item?.innerDataArray?.map((obj: any) => obj.title)}
+                  tabsDataArray={item?.innerDataArray?.map(
+                    (obj: any) => obj.title
+                  )}
                 >
-                  {item?.innerDataArray?.map((innerData: any) => <PDF data={innerData.data} onClick={handlePdf} />)}
+                  {item?.innerDataArray?.map(
+                    (innerData: any, index: number) => (
+                      <PDF
+                        key={index}
+                        data={innerData.data}
+                        onClick={handlePdf}
+                      />
+                    )
+                  )}
                 </HorizaontalTabs>
               </Box>
             </div>
           ))}
         </Grid>
       </Grid>
-      {categoryModal &&
+      {categoryModal && (
         <CategoryModal
           requireIcon={categoryIcon}
           open={categoryModal}
           onClose={handleCategoryModal}
           categoryName={categoryData?.title}
-        />}
-      {pdfModal &&
-        <PdfViewModal
-          fileUrl={file}
-          open={pdfModal}
-          onClose={handlePdf}
-        />}
+        />
+      )}
+      {pdfModal && (
+        <PdfViewModal fileUrl={file} open={pdfModal} onClose={handlePdf} />
+      )}
     </>
-  )
+  );
 }
 
-const TitleWithIcon = ({ title, icon, color, currentTab, currentIndex, ...rest }: any) => {
+const TitleWithIcon = ({
+  title,
+  icon,
+  color,
+  currentTab,
+  currentIndex,
+  ...rest
+}: any) => {
   return (
-    <Box sx={styles.titleWithIcon(color, title, currentTab, currentIndex)} {...rest}>
-      <Image src={icon} alt='icon' style={{ margin: '0px auto' }} height={50} width={50} />
+    <Box
+      sx={styles.titleWithIcon(color, title, currentTab, currentIndex)}
+      {...rest}
+    >
+      <Image
+        src={icon}
+        alt="icon"
+        style={{ margin: "0px auto" }}
+        height={50}
+        width={50}
+      />
       <Typography className="title">{title}</Typography>
     </Box>
-  )
+  );
 };
 
 const PDF = ({ data, onClick }: any) => {
   return (
-    <>{data?.map((obj: any, i: number) => (
-      <Box key={i} sx={styles.pdf}>
-        <Image src={PdfIcon} alt='icon' height={30} width={30} />
-        <Typography onClick={() => onClick(obj.file)}>{obj?.title}</Typography>
-      </Box>
-    ))}</>
-  )
-}
+    <>
+      {data?.map((obj: any, i: number) => (
+        <Box key={i} sx={styles.pdf}>
+          <Image src={PdfIcon} alt="icon" height={30} width={30} />
+          <Typography onClick={() => onClick(obj.file)}>
+            {obj?.title}
+          </Typography>
+        </Box>
+      ))}
+    </>
+  );
+};
 
 const styles: any = {
   container: {
     display: { xs: "block", md: "flex" },
   },
   tabRoot: {
-    flexWrap: 'wrap',
-    height: '600px',
-    overflow: 'auto',
+    flexWrap: "wrap",
+    height: "600px",
+    overflow: "auto",
     "& .MuiTab-root": {
       marginRight: "0px !important",
       maxWidth: "unset !important",
       width: "auto !important",
     },
     "& .MuiTabs-flexContainer": {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-between',
-      gap: '10px',
-      '& button.MuiButtonBase-root': {
-        borderRadius: '10px',
-        flexBasis: { lg: '48% !important', md: '49% !important', xs: '100% !important' },
-      }
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "space-between",
+      gap: "10px",
+      "& button.MuiButtonBase-root": {
+        borderRadius: "10px",
+        flexBasis: {
+          lg: "48% !important",
+          md: "49% !important",
+          xs: "100% !important",
+        },
+      },
     },
   },
   tabIndicator: {
@@ -168,61 +202,96 @@ const styles: any = {
     fontSize: "12px !important",
     lineHeight: "14.4px !important",
     fontWeight: 600,
-    letterSpacing: '0.005em',
-    textAlign: 'center !important'
+    letterSpacing: "0.005em",
+    textAlign: "center !important",
   },
   tabLabel: {
     display: "flex",
-    flexDirection: 'column',
+    flexDirection: "column",
     alignItems: "center",
-    justifyContent: 'center',
+    justifyContent: "center",
     borderRadius: 3,
     padding: "12px",
     gap: "15px",
   },
-  titleWithIcon: (backgroundColor: string, title: string, currentTab: number, currentIndex: number) => ({
-    height: '120px',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: '10px',
-    backgroundColor: currentTab === currentIndex && title !== 'Add More' ? backgroundColor : '#fff',
-    gap: '5px',
-    p: '12px 9px',
-    '& .title': {
-      color: currentTab === currentIndex && title !== 'Add More' ? '#fff' : backgroundColor,
+  titleWithIcon: (
+    backgroundColor: string,
+    title: string,
+    currentTab: number,
+    currentIndex: number
+  ) => ({
+    height: "120px",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor:
+      currentTab === currentIndex && title !== "Add More"
+        ? backgroundColor
+        : "#fff",
+    gap: "5px",
+    p: "12px 9px",
+    "& .title": {
+      color:
+        currentTab === currentIndex && title !== "Add More"
+          ? "#fff"
+          : backgroundColor,
       fontWeight: 600,
-      fontSize: '12px',
-      marginTop: '20px'
+      fontSize: "12px",
+      marginTop: "20px",
     },
-    '& img': {
-      filter: currentTab === currentIndex && title !== 'Add More' ?
-        `brightness(${title === 'Stakeholder Surveys' ? '1' : '0'}) invert(${title === 'Stakeholder Surveys' ? '0' : '1'})`
-        : `brightness(1) invert(0)`,
+    "& img": {
+      filter:
+        currentTab === currentIndex && title !== "Add More"
+          ? `brightness(${
+              title === "Stakeholder Surveys" ? "1" : "0"
+            }) invert(${title === "Stakeholder Surveys" ? "0" : "1"})`
+          : `brightness(1) invert(0)`,
     },
-    ':hover': {
+    ":hover": {
       backgroundColor,
-      '& img': {
-        filter: `brightness(${title === 'Stakeholder Surveys' || title === 'Add More' ? 1 : 0}) 
-                invert(${title === 'Stakeholder Surveys' || title === 'Add More' ? 0 : 1})`,
-        path: { fill: '#fff', }
+      "& img": {
+        filter: `brightness(${
+          title === "Stakeholder Surveys" || title === "Add More" ? 1 : 0
+        }) 
+                invert(${
+                  title === "Stakeholder Surveys" || title === "Add More"
+                    ? 0
+                    : 1
+                })`,
+        path: { fill: "#fff" },
       },
-      '& .title': {
-        color: '#fff',
+      "& .title": {
+        color: "#fff",
       },
-    }
+    },
   }),
-  addNewIcon: { position: 'absolute', top: 13, right: 10, zIndex: 10, cursor: 'pointer' },
-  horizontalTab: {
-    height: '730px', position: 'relative', overflow: 'auto', '& .MuiPaper-root': {
-      height: '100%',
-      '& .MuiBox-root': {
-        maxHeight: '95%',
-        overflow: 'auto',
-      }
-    }
+  addNewIcon: {
+    position: "absolute",
+    top: 13,
+    right: 10,
+    zIndex: 10,
+    cursor: "pointer",
   },
-  pdf: { display: 'flex', alignItems: 'center', gap: '10px', cursor: 'pointer', my: 1, }
-}
+  horizontalTab: {
+    height: "730px",
+    position: "relative",
+    overflow: "auto",
+    "& .MuiPaper-root": {
+      height: "100%",
+      "& .MuiBox-root": {
+        maxHeight: "95%",
+        overflow: "auto",
+      },
+    },
+  },
+  pdf: {
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    cursor: "pointer",
+    my: 1,
+  },
+};
