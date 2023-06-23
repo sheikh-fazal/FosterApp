58855e2610d3f93337587fc2d3b1ed605828651d
arsalan <arsalan.tahir@ceative.co.uk>
2023-06-23 11:51:28 +0500

    #comment compliance-management  and children guides done

--- src/sections/children-guides/index.tsx
+++ src/sections/children-guides/index.tsx
@@ -1,21 +1,18 @@
-
-
-const pdf = './dummyPdf.pdf';
-
+import pdfFile from '../../components/modal/PdfViewModal/dummy.pdf';
 export const ChildrenGuidesArray = [
   {
     id: '1',
     title: 'XYZ Children’s Guide',
-    file: pdf
+    file: pdfFile
   },
   {
     id: '2',
     title: 'XYZ Children’s Guide',
-    file: pdf
+    file: pdfFile
   },
   {
     id: '3',
     title: 'XYZ Children’s Guide',
-    file: pdf
+    file: pdfFile
   },
 ]
--- src/sections/children-guides/useChildrenGuides.ts
+++ src/sections/children-guides/useChildrenGuides.ts
@@ -2,9 +2,26 @@ import { useState } from "react";
 
 export const useChildrenGuides = () => {
   const [addModal, setAddModal] = useState(false);
+  const [pdfModal, setPdfModal] = useState(false);
+  const [pdfFile, setPdfFile] = useState(null);
+
   const handleModal = () => setAddModal(!addModal);
+
+  const handleOpenPdfModal = (file: any) => {
+    setPdfModal(true);
+    setPdfFile(file);
+  };
+
+  const handleClosePdfModal = () => {
+    setPdfModal(false);
+    setPdfFile(null);
+  }
   return {
     addModal,
-    handleModal
+    pdfFile,
+    pdfModal,
+    handleModal,
+    handleOpenPdfModal,
+    handleClosePdfModal
   }
 }
