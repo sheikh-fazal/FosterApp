07e3970d478e6ea03fbfdde63e38875bf1496331
arsalan <arsalan.tahir@ceative.co.uk>
2023-07-03 11:50:43 +0500

    #comment de-reg initial-request first two view form added

---
+++ src/sections/de-registration/deregister-foster-dashboard/dereg-initial-request/deregister-sw-assessment/index.tsx
@@ -0,0 +1,17 @@
+export const SWAssessmentFormData = [
+  {
+    srNo: '1',
+    id:'1',
+    assessmentItem: 'uncontested resignation',
+    status: 'Passed',
+    socialWorker: 'XYZ'
+  }
+];
+
+export const SELECT_FILTERS = [
+  {
+    key: "status",
+    label: "Status",
+    options: [{ label: "Passed", value: "Passed" }],
+  },
+];
