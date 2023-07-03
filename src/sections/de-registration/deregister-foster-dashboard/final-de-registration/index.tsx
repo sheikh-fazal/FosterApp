b4a20b81f3965a3f5a0086f61cf7e22413a6a7db
Hassan Khokhar <67870445+mhassan7599@users.noreply.github.com>
2023-07-03 09:17:47 +0500

    #comment changing done

--- src/sections/de-registration/deregister-foster-dashboard/final-de-registration/index.tsx
+++ src/sections/de-registration/deregister-foster-dashboard/final-de-registration/index.tsx
@@ -8,13 +8,11 @@ export const DeRegContractsAgreementsData = [
     id: 1,
     informationIcon: [
       {
-        content: `A report must be presented to the Fostering Panel on the foster carer's first review for a recommendation and thereafter may be presented to the fostering panel every three years. West Berkshire Children and Family Service complete the First Foster Care Review within 6 months of the carers approval. Other reasons to return to panel would include the following: where significant changes to the foster carer's approval or the termination of the foster carer's approval are recommended or where any circumstances exist which in the opinion of the Agency Decision Maker require consideration by the Fostering Panel. This may include the situation where an updating Disclosure and Barring Service check reveals a new concern about the foster carer or a member of the household.
-    `,
+        content: `The Agency Decision Maker will be sent a copy of all papers considered by the panel when reaching their recommendation.`,
 
       },
       {
-        content: `Whether presented to the fostering panel or not, the report will be presented to the Agency Decision Maker for a decision as to whether the foster carer remains suitable to foster and whether the terms of approval remain suitable. Where it has been presented to the fostering panel, the Agency Decision Maker will take into account the panel's recommendation.
-    `,
+        content: `Based on the Recommendation ,Agency Decision Maker (ADM ) will either approve or reject the Foster Carer`,
 
       },
     ],
