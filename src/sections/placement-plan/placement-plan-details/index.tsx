cef5b351e950dfa0fcf643efd094b09b114a970d
arsalan <arsalan.tahir@ceative.co.uk>
2023-07-04 16:05:32 +0500

    working on placement plan

---
+++ src/sections/placement-plan/placement-plan-details/index.tsx
@@ -0,0 +1,114 @@
+export const FosteringPlacementData = [
+  {
+    title: 'SECTION A: Placement Plan for Fostering Placements',
+    color: '#0E918C',
+    subArr: [
+      {
+        title: 'Details of child/young person',
+        href: '/placement-plan/placement-plan-details/fostering-placement/child-young-person',
+      },
+      {
+        title: 'Details of key people',
+        href: '',
+      },
+      {
+        title: 'Placement details',
+        href: '/placement-plan/placement-plan-details/fostering-placement/placement-details',
+      },
+      {
+        title: 'Living together',
+        href: '/placement-plan/placement-plan-details/fostering-placement/living-together',
+      },
+      {
+        title: 'Self care skills/Preparing for independence',
+        href: '/placement-plan/placement-plan-details/fostering-placement/self-care-skills',
+      },
+      {
+        title: 'Social and leisure activities',
+        href: '/placement-plan/placement-plan-details/fostering-placement/social-and-leisure-activities',
+      },
+      {
+        title: 'Health: Details of key people',
+        href: '',
+      },
+      {
+        title: 'Health: Details of health issues',
+        href: '',
+      },
+      {
+        title: 'Education-related contacts',
+        href: '',
+      },
+      {
+        title: 'Educational needs',
+        href: '',
+      },
+      {
+        title: 'Identity ',
+        href: '',
+      },
+      {
+        title: 'Contact ',
+        href: '',
+      },
+      {
+        title: 'Support for carers ',
+        href: '',
+      },
+      {
+        title: 'Action sheet',
+        href: '',
+      },
+      {
+        title: 'Information checklist for foster carer',
+        href: '',
+      },
+      {
+        title: 'Signature page',
+        href: '',
+      },
+    ]
+  },
+  {
+    title: 'SECTION B: Delegated Authority - Decision Support Tool',
+    color: '#f6830f',
+    subArr: [
+      {
+        title: 'What is Decision Support Tool?',
+        href: '',
+      },
+      {
+        title: 'Medical and health ',
+        href: '',
+      },
+      {
+        title: 'Education ',
+        href: '',
+      },
+      {
+        title: 'Personal, leisure and home life',
+        href: '',
+      },
+      {
+        title: 'Faith and religious observance',
+        href: '',
+      },
+      {
+        title: 'Identity and names ',
+        href: '',
+      },
+      {
+        title: 'Contact',
+        href: '',
+      },
+      {
+        title: 'Other areas or categories',
+        href: '',
+      },
+      {
+        title: 'Additional notes or questions',
+        href: '',
+      },
+    ]
+  },
+]
