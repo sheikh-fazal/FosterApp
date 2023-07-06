ecc1ddde6b4c557bc5d2c14f5f053980645e1b97
Ayaz-khan <ayaz.khan@ceative.co.uk>
2023-07-06 15:01:18 +0500

    overflowed data is added to accordions inner components tables.

--- src/sections/marketing/email-marketing/email-tab/meet-our-new-head-of-safeguarding/MeetOurNewHeadOfSafeguarding.tsx
+++ src/sections/marketing/email-marketing/email-tab/meet-our-new-head-of-safeguarding/MeetOurNewHeadOfSafeguarding.tsx
@@ -56,8 +56,8 @@ const MeetOurNewHeadOfSafeguarding = () => {
           {
             inputType: "textField",
             type: "text",
-            key: "status",
-            defaultValue: "Draco Malfoy ",
+            key: "status", 
+            defaultValue: "pending",
             label: "Status",
             validation: (Yup: any) => {
               return Yup.string().required("Field is required")
@@ -66,8 +66,8 @@ const MeetOurNewHeadOfSafeguarding = () => {
           {
             inputType: "textField",
             type: "text",
-            key: "linkToDraft",
-            defaultValue: "Draco Malfoy ",
+            key: "linkToDraft", 
+            defaultValue: "draft link",
             label: "Link To Draft",
             validation: (Yup: any) => {
               return Yup.string().required("Field is required")
@@ -77,7 +77,7 @@ const MeetOurNewHeadOfSafeguarding = () => {
             inputType: "textField",
             type: "text",
             key: "goal",
-            defaultValue: "Draco Malfoy ",
+            defaultValue: "Write goal",
             label: "Goal",
             validation: (Yup: any) => {
               return Yup.string().required("Field is required")
@@ -87,8 +87,137 @@ const MeetOurNewHeadOfSafeguarding = () => {
             inputType: "textField",
             type: "text",
             key: "sendListName",
-            defaultValue: "Draco Malfoy ",
+            defaultValue: "list name",
             label: "Send List Name",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "imageAssets",
+            defaultValue: "link",
+            label: "Image Assets",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "landingPage",
+            defaultValue: "link",
+            label: "Landing Page",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "facebookShareLink",
+            defaultValue: "link",
+            label: "Facebook Share Link",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "twitterShareLink",
+            defaultValue: "link",
+            label: "Twitter Share Link",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "UTMcampaign",
+            defaultValue: "fotH352305923",
+            label: "UTM Campaign",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "totalEmailSent",
+            defaultValue: "1256213",
+            label: "Total Email Sent",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "totalEmailDelivered",
+            defaultValue: "215235",
+            label: "Total Email Delivered",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },          {
+            inputType: "textField",
+            type: "text",
+            key: "totalEmailOpened",
+            defaultValue: "2124",
+            label: "Total Email Opened",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "totalClicked",
+            defaultValue: "42",
+            label: "Total Clicked",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "deliverablitiy",
+            defaultValue: "90%",
+            label: "Deliverability",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "openRate",
+            defaultValue: "18%",
+            label: "Open Rate",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "CTR",
+            defaultValue: "2.0%",
+            label: "CTR",
+            validation: (Yup: any) => {
+              return Yup.string().required("Field is required")
+            },
+          },
+          {
+            inputType: "textField",
+            type: "text",
+            key: "salesAttributed",
+            defaultValue: "$3072",
+            label: "Sales Attributed",
             validation: (Yup: any) => {
               return Yup.string().required("Field is required")
             },
