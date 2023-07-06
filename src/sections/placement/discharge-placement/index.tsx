688b56c6e6d36e8755988607d3ea3fd55e2969b5
khalid <khalid.mehmood@ceative.co.uk>
2023-07-06 15:51:42 +0500

    child discharge component

---
+++ src/sections/placement/discharge-placement/index.tsx
@@ -0,0 +1,57 @@
+const buttonStyle = {
+    width: "95px",
+    height: "31px",
+    border: "none",
+    borderRadius: "4px",
+    color: "#fff",
+  };
+  
+  // ===================================================
+  
+  export const data = [
+    {
+      id:"1",
+      childName: "Draco Malfoy",
+      carerName: "Tom Hanks",
+      dateDischarge: "12.11.2021",
+      dischargeReport: "",
+      approvedRole: "Albus Dumbledore",
+      localAuthority: "London Borough of Bexley",
+      localAuthoritySW: "London Borough of Bexley",
+      status: <button style={{ ...buttonStyle, background: "#F6830F" }}>Inprogress</button>,
+    },
+    {
+      id:"2",
+      childName: "Draco Malfoy",
+      carerName: "Tom Hanks",
+      dateDischarge: "12.11.2021",
+      dischargeReport: "",
+      approvedRole: "Albus Dumbledore",
+      localAuthority: "London Borough of Bexley",
+      localAuthoritySW: "London Borough of Bexley",
+      status: <button style={{ ...buttonStyle, background: "#0E918C" }}>Complete</button>,
+    },
+    {
+      id:"3",
+      childName: "Draco Malfoy",
+      carerName: "Tom Hanks",
+      dateDischarge: "12.11.2021",
+      dischargeReport: "",
+      approvedRole: "Albus Dumbledore",
+      localAuthority: "London Borough of Bexley",
+      localAuthoritySW: "London Borough of Bexley",
+      status: <button style={{ ...buttonStyle, background: "#0E918C" }}>Complete</button>,
+    },
+    {
+      id:"4",
+      childName: "Draco Malfoy",
+      carerName: "Tom Hanks",
+      dateDischarge: "12.11.2021",
+      dischargeReport: "",
+      approvedRole: "Albus Dumbledore",
+      localAuthority: "London Borough of Bexley",
+      localAuthoritySW: "London Borough of Bexley",
+      status: <button style={{ ...buttonStyle, background: "#0E918C" }}>Complete</button>,
+    },
+  ];
+  
