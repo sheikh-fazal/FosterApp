d20d6f59aca1a6919674d507bb81560f65e16b63
waseem ullah <waseem@ceative.co.uk>
2023-07-07 12:32:18 +0500

    comment query param

--- src/sections/matching-and-placement/matching/recommondation-results/useRecommondationResults.ts
+++ src/sections/matching-and-placement/matching/recommondation-results/useRecommondationResults.ts
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
-import { recommondationResultsDetail, recommondationResultsData } from '.';
+import { recommondationResultsDetail, recommondationResultsData, recommondationResultsDetail2 } from '.';
 import { useRouter } from 'next/router';
 
 export const useRecommondationResults = () => {
-    const router = useRouter()
+    const router = useRouter();
+    const paramValue = router.query.paramName;
+    const {query}:any = useRouter() 
+    const queryValue = query?.viewParam;
+    console.log("query=======> ",query);
+    
+    console.log("queryValue===>",queryValue)
+
+    console.log("currentRoute", paramValue);
     const [selectedCard, setSelectedCard] = useState(null);
     const [filteredDetailData, setFilteredDetailData] = useState(recommondationResultsDetail);
+    const [filteredDetailData1, setFilteredDetailData1] = useState(recommondationResultsDetail2);
     const handleChangeType = (type: any) => {
         setSelectedCard(type);
         const filteredData: any = recommondationResultsDetail.filter(item => item.type === type);
@@ -13,7 +22,8 @@ export const useRecommondationResults = () => {
         console.log("type is ", type)
     }
     return {
-        selectedCard, setSelectedCard, filteredDetailData, setFilteredDetailData, handleChangeType, recommondationResultsData, router
+        selectedCard, setSelectedCard, filteredDetailData, setFilteredDetailData, handleChangeType, recommondationResultsData, router,paramValue
+,filteredDetailData1, setFilteredDetailData1
     }
 
 
