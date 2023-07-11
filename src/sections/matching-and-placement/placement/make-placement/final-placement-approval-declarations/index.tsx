3dfb4669660b59aa62a3a34ec04a4c3bce1a0b76
Hassan Khokhar <67870445+mhassan7599@users.noreply.github.com>
2023-07-11 14:17:24 +0500

    #comment make-placement updated-code

---
+++ src/sections/matching-and-placement/placement/make-placement/final-placement-approval-declarations/index.tsx
@@ -0,0 +1,12 @@
+
+import DocumentIcon from '../../../../../assets/svg/placement/DocumentIcon.svg'
+
+export const finalPlacementApprovalDeclarationData = [
+    {
+        id: 1,
+        name: 'Draco Malfoy',
+        role: 'Foster Carer',
+        dateApproved: '12.11.2021',
+        digitalSignature: 'Agency Approver'
+    }
+]
