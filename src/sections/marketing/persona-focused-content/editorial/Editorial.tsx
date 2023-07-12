7eb23832ab7e7ebe6fb2f77146b1a113c243d90e
Salman khan <salman.khan@ceative.co.uk>
2023-07-12 15:04:56 +0500

    some changes persona focuesd

--- src/sections/marketing/persona-focused-content/editorial/Editorial.tsx
+++ src/sections/marketing/persona-focused-content/editorial/Editorial.tsx
@@ -1,116 +1,285 @@
 // components
 import FormTable from "@root/components/Table/FormTable";
-import { useEditorial } from './useEditorial';
-// form react hook
 import { FormProvider } from "@root/components/hook-form";
-import EditorialTable from "./EditorialTable";
+import dayjs from "dayjs";
+import { Box, Chip } from "@mui/material";
 
-
-const Editorial = () => {
-  const { methods, handleSubmit,  onSubmit, onClear } = useEditorial();
+import { useEditorial } from "./useEditorial";
+import {
+  CONTENTIDEASOPTIONS,
+  PERSONAOPTIONS,
+  PUBLISHEDOPTIONS,
+  PUBLISHOPTIONS,
+  STATUSOPTIONS,
+  VERTICALOPTIONS,
+  WRITEROPTIONS,
+} from ".";
+// form react hook
+function DataChips({ options }: any) {
   return (
-    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <EditorialTable  
-        tableKey="exampleTable"
-        columns={[
-        
-          {
-            inputType: "textField",
-            type: "text",
-            key: "title",
-            defaultValue: "",
-            label: "title",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "status",
-            defaultValue: "",
-            label: "status",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "writer",
-            defaultValue: "",
-            label: "writer",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "publishOn",
-            defaultValue: "",
-            label: "publish On",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-      
-          {
-            inputType: "textField",
-            type: "text",
-            key: "publishLocation",
-            defaultValue: "",
-            label: "publish Location",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "persona",
-            defaultValue: "",
-            label: "persona",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "contentIdeas",
-            defaultValue: "",
-            label: "content Ideas",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "vertical",
-            defaultValue: "",
-            label: "vertical",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "published",
-            defaultValue: "",
-            label: "published",
-            validation: (Yup: any) => {
-              return Yup.string().required("Field is required")
-            },
-          },
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "unwrap",
+        gap: 1,
+      }}
+    >
+      {options
+        .slice(0, 3)
+        .map(
+          ({
+            value,
+            label,
+            bgColor = "#e4e7eb",
+            textColor = "#212b36",
+          }: any) => (
+            <Chip
+              sx={{
+                backgroundColor: bgColor,
+                color: textColor,
+                fontSize: "10px !important",
+                p: "5px 10px",
+                maxHeight: "22px",
 
-        ]}
-      />
-
-    </FormProvider>
+                "& .MuiChip-label": {
+                  p: 0,
+                },
+              }}
+              key={value}
+              label={label}
+            />
+          )
+        )}
+    </Box>
   );
 }
 
-export default Editorial
+const COLUMNS = [
+  {
+    inputType: "textField",
+    type: "text",
+    key: "title",
+    defaultValue: "John",
+    label: "Tilte",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "status",
+    defaultValue: [],
+    label: "Status",
+    options: STATUSOPTIONS,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "writer",
+    defaultValue: [],
+    label: "Writer",
+    options: WRITEROPTIONS,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+
+  {
+    inputType: "datePicker",
+    type: "dob",
+    key: "publishOn",
+    defaultValue: new Date(),
+    label: "Publish On",
+    validation: (Yup: any) => {
+      return Yup.date()
+        .typeError("End date is required")
+        .required("End date is required");
+    },
+    format: (date: any) => {
+      return dayjs(date).format("DD/MM/YYYY");
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "persona",
+    defaultValue: [],
+    label: "Persona",
+    options: PUBLISHOPTIONS,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "PublishLocation",
+    defaultValue: [],
+    label: "Publish Location",
+    options: PERSONAOPTIONS,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "contentIdeas",
+    defaultValue: [],
+    label: "Content Ideas",
+    options: CONTENTIDEASOPTIONS,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "vertical",
+    defaultValue: [],
+    label: "vertical",
+    options: VERTICALOPTIONS,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "published",
+    defaultValue: [],
+    label: "Published",
+    options: PUBLISHEDOPTIONS,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+];
+
+const Editorial = () => {
+  const { methods, handleSubmit, onSubmit, onClear, uploadImage } =
+    useEditorial();
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <FormTable tableKey="exampleTable" columns={COLUMNS} />
+    </FormProvider>
+  );
+};
+
+export default Editorial;
---
+++ src/sections/marketing/persona-focused-content/personas/index.ts
@@ -0,0 +1,101 @@
+import img1 from "../../../../assets/svg/marketing/personasImg1.svg"
+
+
+export const defaultValues = {
+
+  exampleTable: [
+    {
+      name: "Foster Carers",
+      notes:"Loves client case studies wants to see how other companies have solved problems with your product",
+
+      contentIdeas: [
+        {
+          label: "Case Studies",
+          value: "Case Studies",
+          bgColor: "#93C3EF",
+        },
+        {
+          label: "Narratives",
+          value: "Narratives",
+          bgColor: "#8FE0FA",
+        },
+        {
+          label: "Interviews",
+          value: "Interviews",
+          bgColor: "#8FE0FA",
+        },
+      ],
+      attechment:img1,
+      
+          editorial: [
+        {
+          label: "5 Keys to Success With SEO",
+          value: "5 Keys to Success With SEO",
+          bgColor: "#C7DDF1",
+        },
+        {
+          label: "Pace Yourself: How Long Your Content should Take to Write",
+          value: "Pace Yourself: How Long Your Content should Take to Write",
+          bgColor: "#C7DDF1",
+        },
+      
+      ],
+      vertical: [
+        {
+          label: "Real Estate",
+          value: "Real Estate",
+          bgColor: "#C7DDF1",
+        },
+      ],
+    },
+  ],}
+
+  export const CONTENTOPTIONS = [
+    {
+      label: "Case Studies",
+      value: "Case Studies",
+      bgColor: "#93C3EF",
+    },
+    {
+      label: "Narratives",
+      value: "Narratives",
+      bgColor: "#8FE0FA",
+    },
+    {
+      label: "Interviews",
+      value: "Interviews",
+      bgColor: "#8FE0FA",
+    },
+    
+  ];
+
+  export const EDITRIALOPTIONS = [
+    {
+      label: "5 Keys to Success With SEO",
+      value: "5 Keys to Success With SEO",
+      bgColor: "#C7DDF1",
+    },
+    {
+      label: "Pace Yourself: How Long Your Content should Take to Write",
+      value: "Pace Yourself: How Long Your Content should Take to Write",
+      bgColor: "#C7DDF1",
+    },
+
+  ]
+  export  const VERTICALOPTIONS= [
+    {
+      label: "Real Estate",
+      value: "Real Estate",
+      bgColor: "#C7DDF1",
+    },
+    {
+      label: "Healthcare",
+      value: "Healthcare",
+      bgColor: "#C7DDF1",
+    },
+    {
+      label: "Retail",
+      value: "Retail",
+      bgColor: "#C7DDF1",
+    },
+  ]
