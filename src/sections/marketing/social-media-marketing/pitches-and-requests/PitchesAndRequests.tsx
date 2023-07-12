39e9877c74272a8ed015a49e34c229b0cb968e41
Abubakar Saddiq <abubakar.saddiq@ceative.co.uk>
2023-07-12 11:20:36 +0500

    marketing dynamic table done

--- src/sections/marketing/social-media-marketing/pitches-and-requests/PitchesAndRequests.tsx
+++ src/sections/marketing/social-media-marketing/pitches-and-requests/PitchesAndRequests.tsx
@@ -1,89 +1,267 @@
-import { FormProvider } from "@root/components/hook-form";
-import React from "react";
-import PitchesAndRequestTable from "./pitches-and-request-table/PitchesAndRequestTable";
-import { usePitchesAndRequests } from "./usePitchesAndRequests";
 import FormTable from "@root/components/Table/FormTable";
-import { Box } from "@mui/material";
-import AvatarIMG from "../../../../assets/img/ifaAvatar.png";
-import Image from "next/image";
+import { FormProvider } from "@root/components/hook-form";
+import dayjs from "dayjs";
+import { Box, Button, Chip, Typography } from "@mui/material";
+import { fData } from "@root/utils/formatNumber";
+import MyAvatar from "@root/components/MyAvatar";
+import { usePitchesAndRequests } from "./usePitchesAndRequests";
 
-const PitchesAndRequests = () => {
-  const { methods, handleSubmit, tableData, onSubmit, onClear, route } =
+const MAX_FILE_SIZE = 2 * 1000 * 1000; // 2 Mb
+const FILE_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
+
+///---------------------------------
+// Constants
+
+const TypeOfContent = [
+  {
+    label: "Request",
+    value: "Request",
+    bgColor: "#BEA2FA",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "UGC",
+    value: "UGC",
+    bgColor: "#A2C5FA",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "Team Social Proposal",
+    value: "Team Social Proposal",
+    bgColor: "#8EEB92",
+    textColor: "#1D1D1D",
+  },
+];
+
+const CHANNELS = [
+  {
+    label: "Twitter",
+    value: "Twitter",
+    bgColor: "#D6ADEA",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "Linkedin",
+    value: "Linkedin",
+    bgColor: "#ADE6EA",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "Instagram",
+    value: "Instagram",
+    bgColor: "#6BD5ED",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "TikTok",
+    value: "TikTok",
+    bgColor: "#E0C06E",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "Facebook",
+    value: "Facebook",
+    bgColor: "#BC6EE0",
+    textColor: "#1D1D1D",
+  },
+];
+
+const COLUMNS = [
+  {
+    inputType: "textField",
+    type: "text",
+    key: "name",
+    defaultValue: "John",
+    label: "Name",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "typeOfContent",
+    defaultValue: [],
+    label: "Type of Content",
+    options: TypeOfContent,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+  {
+    inputType: "textField",
+    type: "text",
+    key: "requestor",
+    defaultValue: "John",
+    label: "Requestor",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "datePicker",
+    type: "dob",
+    key: "requestedDate",
+    defaultValue: new Date(),
+    label: "Requested Date",
+    validation: (Yup: any) => {
+      return Yup.date()
+        .typeError("End date is required")
+        .required("End date is required");
+    },
+    format: (date: any) => {
+      return dayjs(date).format("DD/MM/YYYY");
+    },
+  },
+
+  {
+    inputType: "textField",
+    type: "text",
+    key: "goal",
+    defaultValue:
+      "We have a small launch on 4/25 for new features that will make it easier to edit videos in our app.",
+    label: "Goal",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "file",
+    type: "file",
+    key: "image",
+    label: "Image",
+    size: { xs: 12, md: 12 },
+    // Use this validation for images
+    validation: (Yup: any) => {
+      return Yup.mixed()
+        .test("required", "Image is required", (value: any) => {
+          if (!value) return false;
+          if (typeof value === "string") return !!value;
+          return value.type;
+        })
+        .test("fileFormat", "Unsupported Format", (value: any) => {
+          if (!value) return false;
+          if (typeof value === "string") return !!value;
+          return value && FILE_FORMATS.includes(value.type);
+        })
+        .test(
+          "fileSize",
+          `File must be less than or equal to ${fData(MAX_FILE_SIZE)}`,
+          (value: any) => {
+            if (!value) return false;
+            if (typeof value === "string") return !!value;
+            return value && value.size <= MAX_FILE_SIZE;
+          }
+        );
+    },
+    format: (imgUrl: any) => {
+      if (!!imgUrl)
+        return (
+          <MyAvatar
+            src={String(`${process.env.NEXT_PUBLIC_IMG_URL}${imgUrl}`)}
+            sx={{
+              mx: "auto",
+            }}
+          />
+        );
+
+      return "-";
+    },
+  },
+
+  {
+    inputType: "textField",
+    type: "text",
+    key: "proposedCopy",
+    defaultValue: "Text",
+    label: "Proposed Copy",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "textField",
+    type: "text",
+    key: "teamSocialNotes",
+    defaultValue: "Looks good to me copy needs some work.",
+    label: "Team Social Notes",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+];
+
+///---------------------------------
+// This component is here for testing purposes only
+function DataChips({ options }: any) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "unwrap",
+        gap: 1,
+      }}
+    >
+      {options
+        .slice(0, 3)
+        .map(
+          ({
+            value,
+            label,
+            bgColor = "#e4e7eb",
+            textColor = "#212b36",
+          }: any) => (
+            <Chip
+              sx={{
+                backgroundColor: bgColor,
+                color: textColor,
+                fontSize: "10px !important",
+                p: "5px 10px",
+                maxHeight: "22px",
+
+                "& .MuiChip-label": {
+                  p: 0,
+                },
+              }}
+              key={value}
+              label={label}
+            />
+          )
+        )}
+    </Box>
+  );
+}
+
+///---------------------------------
+
+export default function PitchesAndRequests() {
+  const { methods, handleSubmit, tableData, uploadImage, onSubmit, onClear } =
     usePitchesAndRequests();
+
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <FormTable
         tableKey="exampleTable"
-        route={route}
-        columns={[
-          {
-            inputType: "textField",
-            type: "text",
-            key: "name",
-            defaultValue: "Branded keywords",
-            label: "Name",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "multi-select",
-            type: "text",
-            key: "typeOfContent",
-            defaultValue: "6/1/2023 :: 02:12:12 PM ",
-            label: "Type of Content",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "requestor",
-            defaultValue: "Evergreen Content",
-            label: "Requestor",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "requestedDate",
-            defaultValue: "4/26/2021",
-            label: "Requested Date",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "goal",
-            defaultValue:
-              "Share a unique use of our video platform from an influencer.",
-            label: "Goal",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "proposedCopy",
-            defaultValue: "315",
-            label: "Proposed Copy",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "file",
-            type: "upload",
-            key: "name",
-            label: "Name",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "teamSocialNotes",
-            defaultValue: "315",
-            label: "Team Social Notes",
-            validation: (Yup: any) => {},
-          },
-        ]}
+        beforeAdd={(methods: any) => uploadImage("image", methods)}
+        beforeUpdate={(methods: any) => uploadImage("image", methods)}
+        columns={COLUMNS}
       />
     </FormProvider>
   );
-};
-
-export default PitchesAndRequests;
+}
--- src/sections/marketing/social-media-marketing/results/Results.tsx
+++ src/sections/marketing/social-media-marketing/results/Results.tsx
@@ -1,86 +1,204 @@
-import React from "react";
-import { FormProvider } from "@root/components/hook-form";
-import { useResults } from "./useResults";
-import ResultTable from "./result-table/ResultTable";
 import FormTable from "@root/components/Table/FormTable";
+import { FormProvider } from "@root/components/hook-form";
+import dayjs from "dayjs";
+import { Box, Button, Chip, Typography } from "@mui/material";
+import { fData } from "@root/utils/formatNumber";
+import MyAvatar from "@root/components/MyAvatar";
+import { useResults } from "./useResults";
 
-const Results = () => {
-  const { methods, handleSubmit, tableData, onSubmit, onClear, route } =
+const MAX_FILE_SIZE = 2 * 1000 * 1000; // 2 Mb
+const FILE_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
+
+///---------------------------------
+// Constants
+
+const TYPEOFPOST = [
+  {
+    label: "Organic",
+    value: "Organic",
+    bgColor: "#BEA2FA",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "Paid Campaign",
+    value: "Paid Campaign",
+    bgColor: "#89CDFF",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "Evergreen Content",
+    value: "Evergreen Content",
+    bgColor: "#8CFFA5",
+    textColor: "#1D1D1D",
+  },
+  {
+    label: "Launch",
+    value: "Launch",
+    bgColor: "#CDFF8C",
+    textColor: "#1D1D1D",
+  },
+];
+
+
+
+const COLUMNS = [
+  {
+    inputType: "textField",
+    type: "text",
+    key: "postTopic",
+    defaultValue: "John",
+    label: "Post Topic",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "datePicker",
+    type: "dob",
+    key: "publishDate",
+    defaultValue: new Date(),
+    label: "Publish Date",
+    validation: (Yup: any) => {
+      return Yup.date()
+        .typeError("End date is required")
+        .required("End date is required");
+    },
+    format: (date: any) => {
+      return dayjs(date).format("DD/MM/YYYY");
+    },
+  },
+  {
+    inputType: "multi-select",
+    type: "select",
+    key: "TypeOfPost",
+    defaultValue: [],
+    label: "Type of Post",
+    options: TYPEOFPOST,
+    validation: (Yup: any) => {
+      return Yup.array()
+        .of(
+          Yup.object().shape({
+            label: Yup.string(),
+            value: Yup.string(),
+            bgColor: Yup.string(),
+            textColor: Yup.string(),
+          })
+        )
+        .test(
+          "required",
+          "Platform is required.",
+          (arr: any) => arr.length > 0
+        );
+    },
+    format: (selectedValues = []) => {
+      return <DataChips options={selectedValues} />;
+    },
+  },
+  {
+    inputType: "textField",
+    type: "text",
+    key: "impressions",
+    defaultValue: "10756",
+    label: "Impressions",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "textField",
+    type: "text",
+    key: "engagements",
+    defaultValue: "39542",
+    label: "Engagements",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "textField",
+    type: "text",
+    key: "engagementsRate",
+    defaultValue: "2.18%",
+    label: "Engagements Rate",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "textField",
+    type: "text",
+    key: "spend",
+    defaultValue: "$0.00",
+    label: "Spend",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+  {
+    inputType: "textField",
+    type: "text",
+    key: "costPerEngagement",
+    defaultValue: "$0.00",
+    label: "Cost Per Engagement",
+    validation: (Yup: any) => {
+      return Yup.string().required("Name is required").min(3);
+    },
+  },
+];
+
+///---------------------------------
+// This component is here for testing purposes only
+function DataChips({ options }: any) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "unwrap",
+        gap: 1,
+      }}
+    >
+      {options
+        .slice(0, 3)
+        .map(
+          ({
+            value,
+            label,
+            bgColor = "#e4e7eb",
+            textColor = "#212b36",
+          }: any) => (
+            <Chip
+              sx={{
+                backgroundColor: bgColor,
+                color: textColor,
+                fontSize: "10px !important",
+                p: "5px 10px",
+                maxHeight: "22px",
+
+                "& .MuiChip-label": {
+                  p: 0,
+                },
+              }}
+              key={value}
+              label={label}
+            />
+          )
+        )}
+    </Box>
+  );
+}
+
+///---------------------------------
+
+export default function Results() {
+  const { methods, handleSubmit, tableData, onSubmit, onClear } =
     useResults();
+
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <FormTable
         tableKey="exampleTable"
-        route={route}
-        columns={[
-          {
-            inputType: "textField",
-            type: "text",
-            key: "postTopic",
-            defaultValue: "Family-safety-security",
-            label: "Post Topic",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "publishDate",
-            defaultValue: "6/1/2023 :: 02:12:12 PM ",
-            label: "Publish Date",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "typeOfPost",
-            defaultValue: "Evergreen Content",
-            label: "Type of Post",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "impressions",
-            defaultValue: "31534",
-            label: "Impressions",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "engagements",
-            defaultValue: "315",
-            label: "Engagements",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "engagementsRate",
-            defaultValue: "315",
-            label: "Engagements Rate",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "spend",
-            defaultValue: "315",
-            label: "Spend",
-            validation: (Yup: any) => {},
-          },
-          {
-            inputType: "textField",
-            type: "text",
-            key: "costPerEngagement",
-            defaultValue: "315",
-            label: "Cost Per Engagement",
-            validation: (Yup: any) => {},
-          },
-        ]}
+        columns={COLUMNS}
       />
     </FormProvider>
   );
-};
-
-export default Results;
+}
