bc2c0c7ba1d1447bbe08a6da8020f150b12c8132
Hassan Khokhar <67870445+mhassan7599@users.noreply.github.com>
2023-07-14 09:34:19 +0500

    #comment checklist

---
+++ src/sections/matching-and-placement/placement/placement-meeting-record/index.tsx
@@ -0,0 +1,12 @@
+export const placementMeetingRecordData = [
+    {
+        childName: 'Draco Malfoy',
+        carerName: 'Tom Hanks',
+        meetingDate: '12.11.2021',
+        meetingAttendees: 'Long-Term',
+        meetingAgenda: 'Albus Dumbledore..',
+        agencySocialWorker: 'London Borough of Bexley',
+        localAuthoritySW: 'London Borough of Bexley',
+        voiceMemoRejection: 'From Child'
+    }
+]
