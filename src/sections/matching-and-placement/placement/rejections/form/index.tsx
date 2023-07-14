180c3387169b07ca944b52148fcd32ad6e63b697
Abdullah Sarwar Mustafai <abdullah.sarwar@ceative.co.uk>
2023-07-14 11:05:15 +0500

    #comment rejection-placement, meeting record and checklist added

--- src/sections/matching-and-placement/placement/rejections/form/index.tsx
+++ src/sections/matching-and-placement/placement/rejections/form/index.tsx
@@ -1,117 +1,121 @@
-import { RHFMultiCheckbox, RHFSelect, RHFTextField } from "@root/components/hook-form";
+import {
+  RHFMultiCheckbox,
+  RHFSelect,
+  RHFTextField,
+} from "@root/components/hook-form";
 import RHFDatePicker from "@root/components/hook-form/RHFDatePicker";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const initialValues = {
-  childName: '',
-  carerName: '',
-  whoRejected: '',
-  rejectionDate: '',
-  placementType: '',
-  rejectionReason: '',
-  agencySocialWorker: '',
-  localAuthorityWorker: '',
-  voiceMemoRejection: ''
+  childName: "",
+  carerName: "",
+  whoRejected: "",
+  rejectionDate: new Date(),
+  placementType: "",
+  rejectionReason: "",
+  agencySocialWorker: "",
+  localAuthorityWorker: "",
+  voiceMemoRejection: "",
 };
 
 export const formSchema = Yup.object().shape({
-  childName: Yup.string().required('Field is required'),
-  carerName: Yup.string().required('Field is required'),
-  whoRejected: Yup.string().required('Field is required'),
-  rejectionDate: Yup.string().required('Field is required'),
-  placementType: Yup.string().required('Field is required'),
-  rejectionReason: Yup.string().required('Field is required'),
-  agencySocialWorker: Yup.string().required('Field is required'),
-  localAuthorityWorker: Yup.string().required('Field is required'),
-  voiceMemoRejection: Yup.string().required('Field is required')
-})
+  childName: Yup.string().required("Field is required"),
+  carerName: Yup.string().required("Field is required"),
+  whoRejected: Yup.string().required("Field is required"),
+  rejectionDate: Yup.string().required("Field is required"),
+  placementType: Yup.string().required("Field is required"),
+  rejectionReason: Yup.string().required("Field is required"),
+  agencySocialWorker: Yup.string().required("Field is required"),
+  localAuthorityWorker: Yup.string().required("Field is required"),
+  voiceMemoRejection: Yup.string().required("Field is required"),
+});
 
 export const formData = [
   {
     gridLength: 6,
     otherOptions: {
-      label: 'Child Name',
-      name: 'childName',
+      label: "Child Name",
+      name: "childName",
       fullWidth: true,
     },
-    component: RHFTextField
+    component: RHFTextField,
   },
   {
     gridLength: 6,
     otherOptions: {
-      label: 'Carer Name',
-      name: 'carerName',
+      label: "Carer Name",
+      name: "carerName",
       fullWidth: true,
     },
-    component: RHFTextField
+    component: RHFTextField,
   },
   {
     gridLength: 6,
-    title: 'Who Rejected?',
+    title: "Who Rejected?",
     otherOptions: {
       name: "whoRejected",
-      options: ["Child", "Carer", "LA SSW", 'SSW'],
+      options: ["Child", "Carer", "LA SSW", "SSW"],
     },
-    sx: { '& .MuiFormGroup-root': { flexDirection: 'row !important' } },
-    component: RHFMultiCheckbox
+    sx: { "& .MuiFormGroup-root": { flexDirection: "row !important" } },
+    component: RHFMultiCheckbox,
   },
   {
     gridLength: 6,
     otherOptions: {
-      label: 'Rejecteion Date',
-      name: 'rejectionDate',
+      label: "Rejecteion Date",
+      name: "rejectionDate",
       fullWidth: true,
     },
-    component: RHFDatePicker
+    component: RHFDatePicker,
   },
   {
     gridLength: 6,
     otherOptions: {
-      label: 'Placement Type',
-      name: 'placementType',
-      fullWidth: true,
-      select: true,
-      options: [{ value: 'value', label: 'label' }]
-    },
-    component: RHFSelect
-  },
-  {
-    gridLength: 6,
-    otherOptions: {
-      label: 'Rejection Reason',
-      name: 'rejectionReason',
-      fullWidth: true,
-    },
-    component: RHFTextField
-  },
-  {
-    gridLength: 6,
-    otherOptions: {
-      label: 'Agency Social Worker (SSW)',
-      name: 'agencySocialWorker',
+      label: "Placement Type",
+      name: "placementType",
       fullWidth: true,
       select: true,
-      options: [{ value: 'value', label: 'label' }]
+      options: [{ value: "value", label: "label" }],
     },
-    component: RHFSelect
+    component: RHFSelect,
   },
   {
     gridLength: 6,
     otherOptions: {
-      label: 'Local Authority Worker SW',
-      name: 'localAuthorityWorker',
+      label: "Rejection Reason",
+      name: "rejectionReason",
+      fullWidth: true,
+    },
+    component: RHFTextField,
+  },
+  {
+    gridLength: 6,
+    otherOptions: {
+      label: "Agency Social Worker (SSW)",
+      name: "agencySocialWorker",
       fullWidth: true,
       select: true,
-      options: [{ value: 'value', label: 'label' }]
+      options: [{ value: "value", label: "label" }],
     },
-    component: RHFSelect
+    component: RHFSelect,
   },
   {
     gridLength: 6,
     otherOptions: {
-      label: 'Voice Memo on Rejection',
-      name: 'voiceMemoRejection',
+      label: "Local Authority Worker SW",
+      name: "localAuthorityWorker",
+      fullWidth: true,
+      select: true,
+      options: [{ value: "value", label: "label" }],
+    },
+    component: RHFSelect,
+  },
+  {
+    gridLength: 6,
+    otherOptions: {
+      label: "Voice Memo on Rejection",
+      name: "voiceMemoRejection",
       fullWidth: true,
     },
   },
-]
+];
