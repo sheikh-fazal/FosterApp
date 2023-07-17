aedc8a36c78d92f1b7b33a34d2d8cb36ffcd502b
Salman khan <salman.khan@ceative.co.uk>
2023-07-17 08:31:05 +0500

    merge with abubakr

--- src/sections/system-admin/backup-and-archive-settings/ArchiveSettings.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ArchiveSettings = () => {
-  return (
-    <div>ArchiveSettings</div>
-  )
-}
-
-export default ArchiveSettings
--- src/sections/system-admin/notification-management/NotificationManagement.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotificationManagement = () => {
-  return (
-    <div>NotificationManagement</div>
-  )
-}
-
-export default NotificationManagement
