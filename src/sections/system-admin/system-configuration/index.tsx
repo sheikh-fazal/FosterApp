8a4240288de1a70cee8796bd12fd08089ed68e1e
waseem ullah <waseem@ceative.co.uk>
2023-07-18 10:40:23 +0500

    table mock data

---
+++ src/sections/system-admin/system-configuration/index.tsx
@@ -0,0 +1,38 @@
+export const systemConfigurationData = [
+    {
+        id:'1',
+        configurationName:'Number of Child per Carer',
+        configurationValue:'4',
+        description:'Maximum number of child allowed per Carer ( includes Sibliings )',
+        approver:'Jason Roy',
+        approverRole:'Decision Maker',
+        approvedData:'20/05/2021',
+    },
+    {
+        id:'2',
+        configurationName:'Max Number of Daily logs per day',
+        configurationValue:'15',
+        description:'Maximum number of daily log entry to be entered by the Carer per child',
+        approver:'Richard Smith',
+        approverRole:'Director',
+        approvedData:'20/05/2021',
+    },
+    {
+        id:'3',
+        configurationName:'Min Number of Daily logs per day',
+        configurationValue:'3',
+        description:'Minimum number of daily log entry to be entered by the Carer per child',
+        approver:'Richard Smith',
+        approverRole:'Director',
+        approvedData:'20/05/2021',
+    },
+    {
+        id:'4',
+        configurationName:'Video upload size',
+        configurationValue:'15',
+        description:'Minimum number of daily log entry to be entered by the Carer per child',
+        approver:'Richard Smith',
+        approverRole:'Director',
+        approvedData:'20/05/2021',
+    },
+]
