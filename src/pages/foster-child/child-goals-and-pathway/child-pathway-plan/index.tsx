528673c59d9494f2933c92a8275677108f96e3d4
sheikh faxal <fazal.khurram@ceative.co.uk>
2023-08-01 17:40:52 +0500

    start working on child pathway plan

--- src/pages/foster-child/child-goals-and-pathway/child-pathway-plan/index.tsx
+++ src/pages/foster-child/child-goals-and-pathway/child-pathway-plan/index.tsx
@@ -1,5 +1,29 @@
+import Page from "@root/components/Page";
+import Layout from "@root/layouts";
+import HomeIcon from "@mui/icons-material/Home";
 import React from "react";
 
 export default function ChildPathwayPlan() {
-  return <div>ChildPathwayPlan</div>;
+  const PAGE_TITLE = "Child Pathway Plan";
+
+  const BREADCRUMBS = [
+    {
+      icon: <HomeIcon />,
+      name: "Child Info",
+      href: "/foster-child",
+    },
+    {
+      name: "Pathway Plan List",
+      href: "/",
+    },
+  ];
+  return (
+    <Layout
+      showTitleWithBreadcrumbs
+      breadcrumbs={BREADCRUMBS}
+      title={PAGE_TITLE}
+    >
+      <Page title={PAGE_TITLE}>ChildPathwayPlan</Page>
+    </Layout>
+  );
 }
