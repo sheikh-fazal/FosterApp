f4ce9fe578de6031b565fdd1dc221262a94bd196
sheikh faxal <fazal.khurram@ceative.co.uk>
2023-08-01 21:45:48 +0500

    foster-child working start

--- src/pages/foster-child/child-goals-and-pathway/child-pathway-plan/index.tsx
+++ src/pages/foster-child/child-goals-and-pathway/child-pathway-plan/index.tsx
@@ -2,6 +2,7 @@ import Page from "@root/components/Page";
 import Layout from "@root/layouts";
 import HomeIcon from "@mui/icons-material/Home";
 import React from "react";
+import { ChildPathwayPlanTable } from "@root/sections/foster-child/child-goals-pathway/child-pathway-plan/ChildPathwayPlanTable";
 
 export default function ChildPathwayPlan() {
   const PAGE_TITLE = "Child Pathway Plan";
@@ -23,7 +24,9 @@ export default function ChildPathwayPlan() {
       breadcrumbs={BREADCRUMBS}
       title={PAGE_TITLE}
     >
-      <Page title={PAGE_TITLE}>ChildPathwayPlan</Page>
+      <Page title={PAGE_TITLE}>
+        <ChildPathwayPlanTable />
+      </Page>
     </Layout>
   );
 }
---
+++ src/sections/foster-child/child-goals-pathway/child-pathway-plan/index.tsx
@@ -0,0 +1,32 @@
+export const CHILD_PATHWAYL_PLAN_MOCK_DATA = [
+  {
+    // srNo: 1,
+    id: 1,
+    date: new Date(),
+    pathwayPlanVersion: "12",
+    reviewedby: "helo",
+    author: "helo",
+    consultationTeam: "helo",
+    // comments: "sdsd",
+  },
+  {
+    // srNo: 2,
+    id: 2,
+    date: new Date(),
+    pathwayPlanVersion: "12",
+    reviewedby: "helo",
+    author: "helo",
+    consultationTeam: "helo",
+    // comments: "sdsd",
+  },
+  {
+    // srNo: 3,/
+    id: 3,
+    date: new Date(),
+    pathwayPlanVersion: "12",
+    reviewedby: "helo",
+    author: "helo",
+    consultationTeam: "helo",
+    // comments: "sdsd",
+  },
+];
