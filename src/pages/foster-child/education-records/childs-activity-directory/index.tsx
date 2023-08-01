b7a3f39ef639baf229062afd1c6694cd3ff4147f
talhawajdandanny <talha.wajdan@ceative.co.uk>
2023-08-01 17:41:00 +0500

    #update ChildsActivityDirectory

--- src/pages/foster-child/education-records/childs-activity-directory/index.tsx
+++ src/pages/foster-child/education-records/childs-activity-directory/index.tsx
@@ -1,5 +1,38 @@
+import Layout from "@root/layouts";
 import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import { Box } from "@mui/material";
+import HospitalizationTable from "@root/sections/foster-child/health-medical-history/hospitalization/hospitalizationTable";
+import { TitleWithBreadcrumbLinks } from "@root/components/PageBreadcrumbs";
+import { useRouter } from "next/router";
+
+ChildsActivityDirectory.getLayout = function getLayout(page: any) {
+  return <Layout showTitleWithBreadcrumbs={false}>{page}</Layout>;
+};
 
 export default function ChildsActivityDirectory() {
-  return <div>ChildsActivityDirectory</div>;
+  // Constants
+  const BREADCRUMBS = [
+    {
+      icon: <HomeIcon />,
+      name: "Child’s Activity Directory",
+      href: "/carer-info",
+    },
+    {
+      name: "child Info",
+      href: "",
+    },
+    
+  ];
+  const PAGE_TITLE = "Child’s Activity Directory";
+  return (
+    <Box>
+      <TitleWithBreadcrumbLinks
+        sx={{ mb: 2 }}
+        breadcrumbs={BREADCRUMBS}
+        title={PAGE_TITLE}
+      />
+     
+    </Box>
+  );;
 }
