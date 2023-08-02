baaa59f8b24c6f403ea413ddf92bfddb8fc44144
talhawajdandanny <talha.wajdan@ceative.co.uk>
2023-08-02 21:57:46 +0500

    #update

--- src/sections/foster-child/education-records/childs-activity-directory/ChildActivityDirectory.tsx
+++ src/sections/foster-child/education-records/childs-activity-directory/ChildActivityDirectory.tsx
@@ -11,8 +11,59 @@ import React from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import TuneIcon from "@mui/icons-material/Tune";
 import { cardData } from ".";
+import Link from "next/link";
+import ChildPoper from "./ChildPoper";
+import CancelIcon from "@mui/icons-material/Cancel";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 
 const ChildActivityDirectory = () => {
+  const [filter, setFilter] = React.useState(0);
+  const [renderData, setrenderData] = React.useState([]);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const [anchorElTwo, setAnchorElTwo] = React.useState<HTMLElement | null>(
+    null
+  );
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+  const handleClickTwo = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElTwo(anchorElTwo ? null : event.currentTarget);
+  };
+  const open = Boolean(anchorEl);
+  const openTwo = Boolean(anchorElTwo);
+  const ID = open ? "simple-popper" : undefined;
+  const IdTwo = openTwo ? "simple-popper-2" : undefined;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const showBasedOnAgeGroup = [
+    "Online Classes",
+    "Arts & Crafts",
+    "Pre-School Classes",
+    "Pre-School Signing",
+    "Children’s Center",
+    "Christmas",
+    "Holiday Activities",
+    "Drawing",
+    "Sketching",
+  ];
+  let index = 0;
+  if (filter === 1) {
+    for (const keys of cardData) {
+      if (index < showBasedOnAgeGroup.length) {
+        if (keys.text === showBasedOnAgeGroup[index]) {
+          // setrenderData((old: any) => [...old, keys]);
+          index++;
+        }
+      }
+    }
+  } else if (filter === 2) {
+    console.log("i am here");
+  } else {
+    console.log("i am here else");
+  }
+  console.log(filter);
+
   return (
     <Box mt={2}>
       <Paper variant="elevation" elevation={1} sx={{ borderRadius: 2.5 }}>
@@ -23,7 +74,7 @@ const ChildActivityDirectory = () => {
           px={1}
           pt={1.5}
         >
-          <IconButton disableRipple>
+          <IconButton disableRipple onClick={handleClick}>
             <InfoIcon sx={(theme) => Styles.iconButton(theme)} />
           </IconButton>
           <Typography
@@ -34,7 +85,7 @@ const ChildActivityDirectory = () => {
           >
             Hand picked Kids Activities & Things To Do With Kids
           </Typography>
-          <IconButton disableRipple>
+          <IconButton disableRipple onClick={handleClickTwo}>
             <TuneIcon sx={(theme) => Styles.iconButton(theme)} />
           </IconButton>
         </Box>
@@ -44,23 +95,116 @@ const ChildActivityDirectory = () => {
           container
           justifyContent={{ xs: "center", md: "flex-start" }}
           alignItems={"center"}
+          sx={{
+            "& a": {
+              textDecoration: "none",
+            },
+          }}
         >
-          {cardData.map((items, index) => (
+          {cardData.map((items: any, index: any) => (
             <Grid key={index} item>
-              <Box sx={Styles.MainWapper(items.bgcolor)}>
-                <Typography
-                  variant="body1"
-                  fontSize={14}
-                  color={"white"}
-                  fontWeight={700}
-                >
-                  {items.text}
-                </Typography>
-                <Box sx={Styles.iconWapper(items.bgcolor)}>{items.icon}</Box>
-              </Box>
+              <Link href={items.link}>
+                <Box sx={Styles.MainWapper(items.bgcolor)}>
+                  <Typography
+                    variant="body1"
+                    fontSize={14}
+                    color={"white"}
+                    fontWeight={700}
+                  >
+                    {items.text}
+                  </Typography>
+                  <Box sx={Styles.iconWapper(items.bgcolor)}>{items.icon}</Box>
+                </Box>
+              </Link>
             </Grid>
           ))}
         </Grid>
+        <ChildPoper
+          BoxProps={{ sx: (theme) => Styles.informationStyles(theme) }}
+          PopperProps={{
+            anchorEl: anchorEl,
+            id: ID,
+            open: open,
+            placement: "bottom-start",
+          }}
+        >
+          <Box mb={"auto"}>
+            <IconButton disableRipple onClick={handleClick}>
+              <CancelIcon sx={{ color: "white" }} />
+            </IconButton>
+          </Box>
+
+          <Typography variant="body2" color="white" p={1} px={0}>
+            Take a look at our fantastic featured kids activities, kids clubs,
+            holiday clubs, baby classes and things to do with Kids his week.
+            Want to be featured ? Check out Club Owner Page for out extra
+            publicity packages.
+          </Typography>
+        </ChildPoper>
+        <ChildPoper
+          BoxProps={{ sx: (theme) => Styles.settingStyles(theme) }}
+          PopperProps={{
+            anchorEl: anchorElTwo,
+            id: IdTwo,
+            open: openTwo,
+            placement: "bottom-start",
+          }}
+        >
+          <Box>
+            <FormControlLabel
+              checked={filter === 1 ? true : false}
+              onChange={(props: any) => {
+                console.log(props.target.checked);
+                if (props.target.checked) {
+                  setFilter(1);
+                } else {
+                  setFilter(0);
+                }
+              }}
+              control={<Switch />}
+              label={
+                <Typography
+                  variant="body2"
+                  color={"#343A40"}
+                  fontSize={15}
+                  fontWeight={700}
+                >
+                  Show Based on Age Group
+                </Typography>
+              }
+            />
+
+            <FormControlLabel
+              checked={filter === 2 ? true : false}
+              onChange={(props: any) => {
+                if (props.target.checked) {
+                  setFilter(2);
+                } else {
+                  setFilter(0);
+                }
+              }}
+              control={<Switch />}
+              label={
+                <Typography
+                  variant="body2"
+                  color={"#343A40"}
+                  fontSize={15}
+                  fontWeight={700}
+                >
+                  Show Based on Activity Type
+                </Typography>
+              }
+            />
+          </Box>
+
+          <Box ml={"auto"}>
+            <IconButton disableRipple onClick={handleClickTwo}>
+              <CancelIcon
+                sx={(theme) => ({ color: theme.palette.primary.main })}
+              />
+            </IconButton>
+          </Box>
+        </ChildPoper>
       </Paper>
     </Box>
   );
@@ -98,5 +242,24 @@ const Styles = {
     "& svg": {
       fontSize: 21,
     },
+  }),
+  informationStyles: (theme: Theme) => ({
+    top: "-10px",
+    backgroundColor: theme.palette.primary.main,
+    textColor: "#fff",
+    borderRadius: 4,
+    boxShadow: "2px 4px 7px 0px rgba(14, 145, 140, 0.20)",
+    width: 500,
+    display: "flex",
+  }),
+  settingStyles: (theme: Theme) => ({
+    top: "-10px",
+    backgroundColor: "white",
+    textColor: "#fff",
+    borderRadius: 4,
+    boxShadow: "2px 4px 7px 0px rgba(14, 145, 140, 0.20)",
+    width: 500,
+    display: "flex",
+    padding: "14px 20px",
   }),
 };
--- src/sections/foster-child/education-records/childs-activity-directory/index.tsx
+++ src/sections/foster-child/education-records/childs-activity-directory/index.tsx
@@ -1,4 +1,5 @@
 import IconAnimation from "@root/assets/svg/child-activity-directory/IconAnimation";
+import IconArtsAndCraft from "@root/assets/svg/child-activity-directory/IconArtsAndCraft";
 import IconAttractions from "@root/assets/svg/child-activity-directory/IconAttaractions";
 import IconCalligraphy from "@root/assets/svg/child-activity-directory/IconCalligraphy";
 import IconChildrenCenter from "@root/assets/svg/child-activity-directory/IconChildrenCenter";
@@ -10,96 +11,112 @@ import IconEditing from "@root/assets/svg/child-activity-directory/IconEditing";
 import IconHolidayActivities from "@root/assets/svg/child-activity-directory/IconHolidayActivities";
 import IconOnlineClass from "@root/assets/svg/child-activity-directory/IconOnlineClass";
 import IconPainting from "@root/assets/svg/child-activity-directory/IconPainting";
+import IconPreSchooSigning from "@root/assets/svg/child-activity-directory/IconPreSchooSigning";
 import IconPreSchoolClasss from "@root/assets/svg/child-activity-directory/IconPreSchoolClasss";
 import IconSketching from "@root/assets/svg/child-activity-directory/IconSketching";
 import IconVisualArt from "@root/assets/svg/child-activity-directory/IconVisualArt";
-import IconArtsAndCraft from "@root/assets/svg/child-activity-directory/iconArtsAndCraft";
-import IconPreSchooSigning from "@root/assets/svg/child-activity-directory/iconPreSchooSigning";
 
 export const cardData = [
   {
     text: "Online Classes",
     bgcolor: "#F63D31",
     icon: <IconOnlineClass />,
+    link: "",
   },
   {
     text: "Arts & Crafts",
     bgcolor: "#F63D31",
     icon: <IconArtsAndCraft />,
+    link: "",
   },
   {
     text: "Attractions",
     bgcolor: "#EB9411",
     icon: <IconAttractions />,
+    link: "",
   },
   {
     text: "Pre-School Classes",
     bgcolor: "#20BE60",
     icon: <IconPreSchoolClasss />,
+    link: "",
   },
   {
     text: "Pre-School Signing",
     bgcolor: "#0E918C",
     icon: <IconPreSchooSigning />,
+    link: "",
   },
   {
     text: "Children’s Center",
     bgcolor: "#10B0C6",
     icon: <IconChildrenCenter />,
+    link: "",
   },
   {
     text: "Christmas",
     bgcolor: "#F63D31",
     icon: <IconChristmas />,
+    link: "",
   },
   {
     text: "Holiday Activities",
     bgcolor: "#EB9411",
     icon: <IconHolidayActivities />,
+    link: "",
   },
   {
     text: "Computing",
     bgcolor: "#20BE60",
     icon: <IconComputing />,
+    link: "",
   },
   {
     text: "Drawing",
     bgcolor: "#0E918C",
     icon: <IconDrawing />,
+    link: "",
   },
   {
     text: "Sketching",
     bgcolor: "#10B0C6",
     icon: <IconSketching />,
+    link: "",
   },
   {
     text: "Painting",
     bgcolor: "#465987",
     icon: <IconPainting />,
+    link: "",
   },
   {
     text: "Digital Art",
     bgcolor: "#EB9411",
     icon: <IconDigitalArt />,
+    link: "",
   },
   {
     text: "Calligraphy",
     bgcolor: "#20BE60",
     icon: <IconCalligraphy />,
+    link: "",
   },
   {
     text: "Animation",
     bgcolor: "#0E918C",
     icon: <IconAnimation />,
+    link: "",
   },
   {
     text: "Visual Art",
     bgcolor: "#10B0C6",
     icon: <IconVisualArt />,
+    link: "",
   },
   {
     text: "Editing",
     bgcolor: "#465987",
     icon: <IconEditing />,
+    link: "",
   },
 ];
