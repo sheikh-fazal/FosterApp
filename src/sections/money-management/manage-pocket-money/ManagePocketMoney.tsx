bde2fec8e33757e849133de1ef42f4e8c8167c88
zaka <zaka.ullah@ceative.co.uk>
2023-08-03 22:00:38 +0500

    push

--- src/sections/money-management/manage-pocket-money/ManagePocketMoney.tsx
+++ src/sections/money-management/manage-pocket-money/ManagePocketMoney.tsx
@@ -1,7 +1,21 @@
+import { Paper } from '@mui/material'
 import React from 'react'
 
 export const ManagePocketMoney = () => {
   return (
-    <div>ManagePocketMoney</div>
+    <>
+    <Paper elevation={4} sx={{ padding: 3 }}>
+    <h4> Guide for Pocket Money Management </h4>
+    <h5> Financial Details - 2020/21 </h5>
+    <h5> Guidance for recommended pocket money rates (payable from household element for children in foster care) and savings. </h5>
+
+    <table>
+        <></>
+    </table>
+
+    <h5> Recommended personal allowance rates have been set with the expectation that children in foster care and residential care receive the same level of pocket money.</h5>
+
+    </Paper>
+    </>
   )
 }
