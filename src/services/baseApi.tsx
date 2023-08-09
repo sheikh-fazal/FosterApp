f6f695ef541fa0fe7ffa37fa87ddd2db962448e6
Musharraf Riaz <muhammad.musharraf@ceative.co.uk>
2023-08-09 21:27:53 +0500

    changes in child incidents reportsw

--- src/services/baseApi.tsx
+++ src/services/baseApi.tsx
@@ -80,6 +80,7 @@ export const TAGS = [
   "DIARY_RECORDING_DOCUMENTS",
   "OOHREPORT_LIST_TABLE",
   "REPORTS_DOCUMENTS",
+  "CHILD_REPORTS",
 ];
 // Create our baseQuery instance
 const baseQuery = fetchBaseQuery({
