db38662a17e6f6be3352169c5df91a254883bef8
Ahmer <muhammadahmer.akram@ceative.co.uk>
2023-08-28 21:44:35 +0500

    audit logs api integrated

--- src/sections/system-admin/audit-trails-and-audit-logs/index.tsx
+++ src/sections/system-admin/audit-trails-and-audit-logs/index.tsx
@@ -1,11 +1,60 @@
-export const auditTrialsData = [
+import TableAction from "@root/components/TableAction";
+import dayjs from "dayjs";
+
+export const getAuditTrailsAndLogsColumns = (setOpen: any) => {
+  return [
     {
-        no: '1',
-        name: 'David',
-        eventName: "Change Password",
-        eventTime: "02/04/2022",
-        accountType: "Admin",
-        iPAddresss: "17.172.224.47",
-        status: "Active"
-    }
-]
+      accessorFn: (row: any) => row.name ?? "-",
+      id: "name",
+      cell: (info: any) => info.getValue() ?? "-",
+      header: "Name",
+      isSortable: true,
+    },
+    {
+      accessorFn: (row: any) => row.eventName ?? "-",
+      id: "eventName",
+      cell: (info: any) => info.getValue() ?? "-",
+      header: "Event Name",
+      isSortable: true,
+    },
+    {
+      accessorFn: (row: any) => row.eventDate ?? "-",
+      id: "eventDate",
+      cell: (info: any) => dayjs(info.getValue()).format("MM/DD/YYYY"),
+      header: "Event Time",
+      isSortable: true,
+    },
+    {
+      accessorFn: (row: any) => row.role ?? "-",
+      id: "role",
+      cell: (info: any) => info.getValue() ?? "-",
+      header: "Account Type",
+      isSortable: true,
+    },
+    {
+      accessorFn: (row: any) => row.ipAddress ?? "-",
+      id: "ipAddress",
+      cell: (info: any) => info.getValue() ?? "-",
+      header: "IP Address",
+      isSortable: true,
+    },
+    {
+      accessorFn: (row: any) => row.status ?? "-",
+      id: "status",
+      cell: (info: any) => info.getValue() ?? "-",
+      header: "Status",
+      isSortable: true,
+    },
+    {
+      accessorFn: (row: any) => row.id ?? "-",
+      id: "actions",
+      cell: (info: any) => (
+        <TableAction type="delete" onClicked={() => setOpen(info.getValue())} />
+      ),
+      header: "Actions",
+      isSortable: false,
+    },
+  ];
+};
+
+export { default as AuditLogs } from "./AuditLogs";
